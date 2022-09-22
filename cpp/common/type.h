d7b427657e5d1ff92955ba13d910d6c5490e53fd
mike <pelanek.lukas@seznam.cz>
2022-09-22 15:03:32 +0200

    feat(type): generic hex container type (WIP)

--- cpp/common/type.h
+++ cpp/common/type.h
@@ -6,14 +6,21 @@
 
 namespace carmen {
 
-class Hash {
+template<std::size_t N>
+class HexContainer {
+public:
+    HexContainer<N>(std::array<std::uint8_t, N> data): _data(data) {}
+
+    friend std::ostream& operator<<(std::ostream&, const HexContainer<N>&);
+    friend auto operator<=>(const HexContainer<N>&, const HexContainer<N>&) = default;
+private:
+    std::array<std::uint8_t, N> _data;
+};
+
+class Hash: public HexContainer<32>  {
 
 public:
-    friend std::ostream& operator<<(std::ostream& out, const Hash& hash);
-    friend auto operator<=>(const Hash& hashA, const Hash& hashB) = default;
-    Hash(std::array<std::uint8_t, 32> data): _data(data) {}
-private:
-  std::array<std::uint8_t, 32> _data;
+    Hash(std::array<std::uint8_t, 32> data): HexContainer(data) {}
 };
 
 
