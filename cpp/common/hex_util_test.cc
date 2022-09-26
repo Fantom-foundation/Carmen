47a728028842506ff4bfb3d02b5e06534d209c2c
Lukáš Pelánek <8320012+Mike-CZ@users.noreply.github.com>
2022-09-26 12:40:21 +0200

    Adds base `ByteValue` class for types

---
+++ cpp/common/hex_util_test.cc
@@ -0,0 +1,22 @@
+#include "common/hex_util.h"
+
+#include <ostream>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace carmen {
+namespace {
+
+using ::testing::StrEq;
+
+TEST(HexUtilTest, ContainsAllHexValues) {
+  std::array<uint8_t, 8> values = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab,
+                                           0xcd, 0xef};
+  std::stringstream out;
+  hex_util::WriteTo(out, values);
+  EXPECT_THAT(out.str(), StrEq("0x0123456789abcdef"));
+}
+
+} // namespace
+} // namespace carmen
--- cpp/common/type_test.cc
+++ cpp/common/type_test.cc
@@ -1,13 +1,15 @@
 #include "common/type.h"
 
+#include <sstream>
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include <sstream>
 
 namespace carmen {
 namespace {
 
 using ::testing::StrEq;
+using ::testing::StrNe;
 
 template<typename T>
 std::string Print(const T& value) {
@@ -16,45 +18,35 @@ std::string Print(const T& value) {
     return out.str();
 }
 
-TEST(HashTest, SizeIsCompact) { 
-    // A simple test to ensure that hashes do require a fixed amount of memory.
-    EXPECT_EQ(32, sizeof(Hash)); 
+TEST(ByteValueTest, CanBePrinted) {
+    ByteValue<2> container{0x12, 0xab};
+    EXPECT_THAT(Print(container), StrEq("0x12ab"));
+}
+
+TEST(ByteValueTest, CanBeEmpty) {
+    ByteValue<0> container;
+    EXPECT_THAT(Print(container), StrEq("0x"));
+}
+
+TEST(ByteValueTest, CanBeInitializedEmpty) {
+    ByteValue<1> container;
+    EXPECT_THAT(Print(container), StrEq("0x00"));
+}
+
+TEST(ByteValueTest, CannotHoldMoreValues) {
+    ByteValue<2> container{0x12, 0xab, 0xef};
+    EXPECT_THAT(Print(container), StrNe("0x12abef"));
+}
+
+TEST(HashTest, SizeIsCompact) {
+    EXPECT_EQ(32, sizeof(Hash));
 }
 
 TEST(HashTest, TypeProperties) {
     EXPECT_TRUE(std::is_trivially_copyable_v<Hash>);
     EXPECT_TRUE(std::is_trivially_move_assignable_v<Hash>);
-}
-
-TEST(HashTest, CanBePrinted) {
-    // Test to ensure that hashes are printable in hex format with "0x" prefix
-    Hash hash((std::array<std::uint8_t, 32>{0x12, 0xab}));
-    EXPECT_THAT(Print(hash), StrEq("0x12ab000000000000000000000000000000000000000000000000000000000000"));
-}
-
-TEST(HashTest, CanBeCompared) {
-    // Test to ensure that hashes can be compared
-    Hash hashA((std::array<std::uint8_t, 32>{0x12, 0xab}));
-    Hash hashB((std::array<std::uint8_t, 32>{0x12, 0xab}));
-    Hash hashC((std::array<std::uint8_t, 32>{0x01, 0xab}));
-    EXPECT_EQ(hashA, hashB);
-    EXPECT_NE(hashA, hashC);
-}
-
-TEST(HashTest, AllZerosBinary) {
-    // Test to ensure that hashes can contain only zero binary values
-    std::array<std::uint8_t, 32> data;
-    data.fill(0);
-    Hash hash((data));
-    EXPECT_THAT(Print(hash), StrEq("0x0000000000000000000000000000000000000000000000000000000000000000"));
-}
-
-TEST(HashTest, AllOnesBinary) {
-    // Test to ensure that hashes can contain only one values
-    std::array<std::uint8_t, 32> data;
-    data.fill(255);
-    Hash hash((data));
-    EXPECT_THAT(Print(hash), StrEq("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+    EXPECT_TRUE(std::equality_comparable<Hash>);
+    EXPECT_TRUE(std::is_default_constructible_v<Hash>);
 }
 
 } // namespace
