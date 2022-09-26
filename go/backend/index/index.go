a5cab1f3c91f8f595f702b289020da6fd277c0ba
Kamil Jezek <kamil.jezek@gmail.com>
2022-09-26 09:08:22 +0200

    adds comments to the index interface

--- go/backend/index/index.go
+++ go/backend/index/index.go
@@ -9,9 +9,16 @@ import "github.com/Fantom-foundation/Carmen/go/common"
 // be hashed and compared. The type I is the type used for the
 // ordinal numbers.
 type Index[K comparable, I common.Identifier] interface {
+
+	// GetOrAdd returns an index mapping for the key, or creates the new index
 	GetOrAdd(key K) (I, error)
+
+	// Contains returns a bool flag to test existence of the key in the mapping
 	Contains(key K) bool
 
+	// GetStateHash returns the index hash.
 	GetStateHash() common.Hash
+
+	// Close closes the storage and clean-ups all possible dirty values
 	Close() error
 }
