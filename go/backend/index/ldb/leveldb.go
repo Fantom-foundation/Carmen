e371f6b5903f4980a898dbc6b747b274096bf39a
Kamil Jezek <kamil.jezek@gmail.com>
2022-09-29 18:11:03 +0200

    define enum for table spaces instead of vars

--- go/backend/index/ldb/leveldb.go
+++ go/backend/index/ldb/leveldb.go
@@ -16,7 +16,7 @@ const (
 // KVIndex represents a key-value store for holding the index data.
 type KVIndex[K comparable] struct {
 	db             *leveldb.DB
-	table          []byte
+	table          common.TableSpaces
 	serializer     common.Serializer[K]
 	hashIndex      *index.HashIndex[K]
 	lastIndex      uint32
@@ -24,7 +24,7 @@ type KVIndex[K comparable] struct {
 }
 
 // NewKVIndex creates a new instance of the index backed by a persisted database
-func NewKVIndex[K comparable](db *leveldb.DB, table []byte, serializer common.Serializer[K]) (p *KVIndex[K], err error) {
+func NewKVIndex[K comparable](db *leveldb.DB, table common.TableSpaces, serializer common.Serializer[K]) (p *KVIndex[K], err error) {
 	// read the last hash from the database
 	var hash []byte
 	if hash, err = db.Get(common.AppendKeyStr(table, HashKey), nil); err != nil {
--- go/common/scheme.go
+++ go/common/scheme.go
@@ -1,22 +1,27 @@
 package common
 
-var (
+// TableSpaces divide key-value storage into spaces by adding a prefix to the key.
+type TableSpaces byte
+
+const (
 	// BalanceKey is a respective "table space"
-	BalanceKey = []byte("B")
+	BalanceKey TableSpaces = 'B'
 	// NonceKey is a respective "table space"
-	NonceKey = []byte("N")
+	NonceKey = 'N'
 	// SlotKey is a respective "table space"
-	SlotKey = []byte("S")
+	SlotKey = 'S'
 	// ValueKey is a respective "table space"
-	ValueKey = []byte("V")
+	ValueKey = 'V'
 )
 
 // AppendKey converts the input key to its respective table space
-func AppendKey(prefix, key []byte) []byte {
-	return append(prefix, key...)
+func AppendKey(prefix TableSpaces, key []byte) []byte {
+	b := []byte{byte(prefix)}
+	return append(b, key...)
 }
 
 // AppendKeyStr converts the input key to its respective table space
-func AppendKeyStr(prefix []byte, key string) []byte {
-	return append(prefix, key...)
+func AppendKeyStr(prefix TableSpaces, key string) []byte {
+	b := []byte{byte(prefix)}
+	return append(b, key...)
 }
