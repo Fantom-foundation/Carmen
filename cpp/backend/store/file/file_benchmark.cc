83a04229a4508a539f465f7eb8bd02a0f90d71f8
Herbert Jordan <herbert@swrnd.ch>
2022-10-04 17:51:37 +0200

    Add file read benchmarks

--- cpp/backend/store/file/file_benchmark.cc
+++ cpp/backend/store/file/file_benchmark.cc
@@ -1,6 +1,7 @@
 
 
 #include <filesystem>
+#include <random>
 #include <sstream>
 
 #include "backend/store/file/file.h"
@@ -9,6 +10,20 @@
 
 namespace carmen::backend::store {
 namespace {
+
+// Contains a range of benchmarks for the File concept implementations.
+// To run benchmarks, use the following command:
+//    bazel run -c opt //backend/store/file:file_benchmark
+//
+// To run subsets of benchmarks, add the filter flag --benchmark_filter=<regex>
+// to the command line. Note that executable parameters must be separated from
+// bazel parameters using a -- parameter. For instance, to only run Read
+// benchmarks on the InMemoryFile implementation us
+//    bazel run -c opt //backend/store/file:file_benchmark --
+//    --benchmark_filter=Read.*InMemory
+//
+// The user Guide for the used benchmark infrastructure can be found here:
+// https://github.com/google/benchmark/blob/main/docs/user_guide.md
 
 // An utility wrapper to be specialized for various file implementations to
 // handle them uniformely within benchmarks.
@@ -83,6 +98,73 @@ BENCHMARK(BM_SequentialFileFilling<SingleFile<4096>>)
     ->Range(kMinSize, kMaxSize);
 BENCHMARK(BM_SequentialFileFilling<SingleFile<16384>>)
     ->Range(kMinSize, kMaxSize);
+
+// A benchmark testing the speed of reading pages sequentially.
+template <typename F>
+void BM_SequentialFileRead(benchmark::State& state) {
+  constexpr static auto kPageSize = F::kPageSize;
+  const auto target_size = state.range(0);
+  using Page = std::array<std::byte, kPageSize>;
+
+  // Create and initialize the test file.
+  FileWrapper<F> wrapper;
+  F& file = wrapper.GetFile();
+  Page trg;
+  const auto num_pages = target_size / kPageSize;
+  file.LoadPage(num_pages - 1, trg);
+
+  for (auto _ : state) {
+    // Load all pages in order.
+    for (std::size_t i = 0; i < num_pages; i++) {
+      file.LoadPage(i, trg);
+      benchmark::DoNotOptimize(trg[0]);
+    }
+  }
+}
+
+BENCHMARK(BM_SequentialFileRead<InMemoryFile<256>>)->Range(kMinSize, kMaxSize);
+BENCHMARK(BM_SequentialFileRead<InMemoryFile<4096>>)->Range(kMinSize, kMaxSize);
+BENCHMARK(BM_SequentialFileRead<InMemoryFile<16384>>)
+    ->Range(kMinSize, kMaxSize);
+
+BENCHMARK(BM_SequentialFileRead<SingleFile<256>>)->Range(kMinSize, kMaxSize);
+BENCHMARK(BM_SequentialFileRead<SingleFile<4096>>)->Range(kMinSize, kMaxSize);
+BENCHMARK(BM_SequentialFileRead<SingleFile<16384>>)->Range(kMinSize, kMaxSize);
+
+// A benchmark testing the speed of reading pages randomly.
+template <typename F>
+void BM_RandomFileRead(benchmark::State& state) {
+  constexpr static auto kPageSize = F::kPageSize;
+  const auto target_size = state.range(0);
+  using Page = std::array<std::byte, kPageSize>;
+
+  // Create and initialize the test file.
+  FileWrapper<F> wrapper;
+  F& file = wrapper.GetFile();
+  Page trg;
+  const auto num_pages = target_size / kPageSize;
+  file.LoadPage(num_pages - 1, trg);
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distribution(0, num_pages - 1);
+
+  for (auto _ : state) {
+    // Load pages in random order.
+    for (std::size_t i = 0; i < num_pages; i++) {
+      file.LoadPage(distribution(gen), trg);
+      benchmark::DoNotOptimize(trg[0]);
+    }
+  }
+}
+
+BENCHMARK(BM_RandomFileRead<InMemoryFile<256>>)->Range(kMinSize, kMaxSize);
+BENCHMARK(BM_RandomFileRead<InMemoryFile<4096>>)->Range(kMinSize, kMaxSize);
+BENCHMARK(BM_RandomFileRead<InMemoryFile<16384>>)->Range(kMinSize, kMaxSize);
+
+BENCHMARK(BM_RandomFileRead<SingleFile<256>>)->Range(kMinSize, kMaxSize);
+BENCHMARK(BM_RandomFileRead<SingleFile<4096>>)->Range(kMinSize, kMaxSize);
+BENCHMARK(BM_RandomFileRead<SingleFile<16384>>)->Range(kMinSize, kMaxSize);
 
 }  // namespace
 }  // namespace carmen::backend::store
