f0d6ae146592828aba0e8d641acdf448bfc02c72
Herbert Jordan <herbert@swrnd.ch>
2022-10-07 12:06:53 +0200

    Add TempDir utilitiy to be used in unit tests

--- cpp/common/file_util.cc
+++ cpp/common/file_util.cc
@@ -16,6 +16,12 @@ std::filesystem::path GetTempFilePath(std::string_view prefix, int i) {
   return std::filesystem::temp_directory_path() / out.str();
 }
 
+std::filesystem::path GetTempDirPath(std::string_view prefix, int i) {
+  std::stringstream out;
+  out << prefix << "_" << i;
+  return std::filesystem::temp_directory_path() / out.str();
+}
+
 }  // namespace
 
 TempFile::TempFile(std::string_view prefix) {
@@ -40,5 +46,28 @@ TempFile::~TempFile() {
 }
 
 const std::filesystem::path& TempFile::GetPath() const { return path_; }
+
+TempDir::TempDir(std::string_view prefix) {
+  // Look for a file name that is not yet used.
+  std::random_device rd;
+  std::default_random_engine rnd(rd());
+  std::uniform_int_distribution<int> dist;
+  path_ = GetTempDirPath(prefix, dist(rnd));
+  while (std::filesystem::exists(path_)) {
+    path_ = GetTempDirPath(prefix, dist(rnd));
+  }
+  // Create that directory to technically allocate that name.
+  // Note: it may happen that a concurrent process is taking ownership of the
+  // same directory at the same time, but since this is a test utility, we
+  // accept that risk.
+  std::filesystem::create_directories(path_);
+}
+
+TempDir::~TempDir() {
+  // Delete the owned temporary file.
+  std::filesystem::remove_all(path_);
+}
+
+const std::filesystem::path& TempDir::GetPath() const { return path_; }
 
 }  // namespace carmen
--- cpp/common/file_util_test.cc
+++ cpp/common/file_util_test.cc
@@ -62,5 +62,66 @@ TEST(TempFile, TheTemporaryFileCanBeRemovedAndRecreatedManually) {
   EXPECT_TRUE(std::filesystem::exists(a.GetPath()));
 }
 
+TEST(TempDir, TypeTraits) {
+  EXPECT_TRUE(std::is_default_constructible_v<TempDir>);
+  EXPECT_FALSE(std::is_copy_constructible_v<TempDir>);
+  EXPECT_FALSE(std::is_move_constructible_v<TempDir>);
+  EXPECT_FALSE(std::is_copy_assignable_v<TempDir>);
+  EXPECT_FALSE(std::is_move_assignable_v<TempDir>);
+}
+
+TEST(TempDir, TheTemporaryDirectoryExistsAfterCreation) {
+  TempDir a;
+  EXPECT_TRUE(std::filesystem::exists(a));
+  EXPECT_TRUE(std::filesystem::is_directory(a));
+}
+
+TEST(TempDir, TheTemporaryDirectoryIsEmpty) {
+  TempDir a;
+  EXPECT_TRUE(std::filesystem::exists(a));
+  int num_entries = 0;
+  for (auto _ : std::filesystem::directory_iterator(a)) {
+    num_entries++;
+  }
+  EXPECT_EQ(0, num_entries);
+}
+
+TEST(TempDir, MultipleTempFilesHaveDifferentPaths) {
+  TempDir a;
+  TempDir b;
+  EXPECT_NE(a.GetPath(), b.GetPath());
+}
+
+TEST(TempDir, TempFileUsedDesiredPrefix) {
+  TempDir a("my_prefix");
+  EXPECT_THAT(a.GetPath().filename(), StartsWith("my_prefix"));
+}
+
+TEST(TempDir, TheTemporaryDirectoryIsAutomaticallyRemoved) {
+  std::filesystem::path path;
+  {
+    TempDir a;
+    path = a.GetPath();
+    EXPECT_TRUE(std::filesystem::exists(path));
+  }
+  EXPECT_FALSE(std::filesystem::exists(path));
+}
+
+TEST(TempDir, ContentOfTemporaryDirectoryIsAutomaticallyRemoved) {
+  std::filesystem::path path;
+  std::filesystem::path file;
+  {
+    TempDir a;
+    path = a.GetPath();
+    EXPECT_TRUE(std::filesystem::exists(path));
+    file = a.GetPath() / "file.dat";
+    ASSERT_FALSE(std::filesystem::exists(file));
+    { std::fstream out(file, std::ios::out); }
+    EXPECT_TRUE(std::filesystem::exists(file));
+  }
+  EXPECT_FALSE(std::filesystem::exists(path));
+  EXPECT_FALSE(std::filesystem::exists(file));
+}
+
 }  // namespace
 }  // namespace carmen
