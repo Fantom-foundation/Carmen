04376a2bc9a95ed3001eb94bd92e2f853b8d175c
Herbert Jordan <herbert@swrnd.ch>
2022-10-18 15:42:30 +0200

    Add Nonce and Balance support to StateDB

--- go/state/state_db_test.go
+++ go/state/state_db_test.go
@@ -1,14 +1,382 @@
 package state
 
 import (
+	"math/big"
 	"testing"
 
+	"github.com/Fantom-foundation/Carmen/go/common"
 	"github.com/golang/mock/gomock"
 )
 
 func TestCaremenStateImplementsStateDbInterface(t *testing.T) {
 	var db stateDB
 	var _ StateDB = &db
+}
+
+func TestCarmenStateGetBalanceReturnsFreshCopy(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Set up the expectation that the store will be called once.
+	want := big.NewInt(12)
+	balance, _ := common.ToBalance(want)
+	mock.EXPECT().GetBalance(gomock.Eq(address1)).Return(balance, nil)
+
+	val1 := db.GetBalance(address1)
+	val2 := db.GetBalance(address1)
+
+	if val1 == val2 {
+		t.Errorf("Did not obtain fresh copy of internal big.Int value")
+	}
+
+	if val1.Cmp(val2) != 0 {
+		t.Errorf("Values of multiple reads do not match: %v vs %v", val1, val2)
+	}
+
+	val1.Add(val1, big.NewInt(1))
+	if val1.Cmp(val2) == 0 {
+		t.Errorf("Failed to modify value 1")
+	}
+
+	val3 := db.GetBalance(address1)
+	if val2.Cmp(val3) != 0 {
+		t.Errorf("Modifying retrieved value changed stored value: %v vs %v", val1, val2)
+	}
+}
+
+func TestCarmenStateBalancesAreReadFromState(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Set up the expectation that the store will be called once.
+	want := big.NewInt(12)
+	balance, _ := common.ToBalance(want)
+	mock.EXPECT().GetBalance(gomock.Eq(address1)).Return(balance, nil)
+
+	if got := db.GetBalance(address1); got.Cmp(want) != 0 {
+		t.Errorf("error retrieving balance, wanted %v, got %v", want, got)
+	}
+}
+
+func TestCarmenStateBalancesAreOnlyReadOnce(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Set up the expectation that the store will be called once.
+	want := big.NewInt(12)
+	balance, _ := common.ToBalance(want)
+	mock.EXPECT().GetBalance(gomock.Eq(address1)).Return(balance, nil)
+
+	if got := db.GetBalance(address1); got.Cmp(want) != 0 {
+		t.Errorf("error retrieving balance, wanted %v, got %v", want, got)
+	}
+	db.GetBalance(address1)
+	db.GetBalance(address1)
+}
+
+func TestCarmenStateBalancesCanBeSnapshottedAndReverted(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Balance is initially 10. This should only be fetched once.
+	want := big.NewInt(10)
+	balance, _ := common.ToBalance(want)
+	mock.EXPECT().GetBalance(gomock.Eq(address1)).Return(balance, nil)
+
+	snapshot0 := db.Snapshot()
+
+	want.SetInt64(10)
+	if got := db.GetBalance(address1); got.Cmp(want) != 0 {
+		t.Errorf("error retrieving balance, wanted %v, got %v", want, got)
+	}
+
+	snapshot1 := db.Snapshot()
+	diff := big.NewInt(2)
+	want.Add(want, diff)
+	db.AddBalance(address1, diff)
+	if got := db.GetBalance(address1); got.Cmp(want) != 0 {
+		t.Errorf("error retrieving balance, wanted %v, got %v", want, got)
+	}
+
+	snapshot2 := db.Snapshot()
+	diff = big.NewInt(3)
+	want.Sub(want, diff)
+	db.SubBalance(address1, diff)
+	if got := db.GetBalance(address1); got.Cmp(want) != 0 {
+		t.Errorf("error retrieving balance, wanted %v, got %v", want, got)
+	}
+
+	db.RevertToSnapshot(snapshot2)
+	want.SetInt64(12)
+	if got := db.GetBalance(address1); got.Cmp(want) != 0 {
+		t.Errorf("error retrieving balance, wanted %v, got %v", want, got)
+	}
+
+	db.RevertToSnapshot(snapshot1)
+	want.SetInt64(10)
+	if got := db.GetBalance(address1); got.Cmp(want) != 0 {
+		t.Errorf("error retrieving balance, wanted %v, got %v", want, got)
+	}
+
+	db.RevertToSnapshot(snapshot0)
+	if got := db.GetBalance(address1); got.Cmp(want) != 0 {
+		t.Errorf("error retrieving balance, wanted %v, got %v", want, got)
+	}
+}
+
+func TestCarmenStateBalanceIsWrittenToStateIfChangedAtEndOfTransaction(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// The balance is expected to be read and the updated value to be written to the state.
+	want := big.NewInt(10)
+	balance, _ := common.ToBalance(want)
+	mock.EXPECT().GetBalance(gomock.Eq(address1)).Return(balance, nil)
+	balance, _ = common.ToBalance(big.NewInt(12))
+	mock.EXPECT().SetBalance(gomock.Eq(address1), gomock.Eq(balance)).Return(nil)
+
+	db.AddBalance(address1, big.NewInt(2))
+	db.EndTransaction()
+}
+
+func TestCarmenStateBalanceOnlyFinalValueIsWrittenAtEndOfTransaction(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Only the last value is to be written to the state.
+	// The balance is expected to be read and the updated value to be written to the state.
+	want := big.NewInt(10)
+	balance, _ := common.ToBalance(want)
+	mock.EXPECT().GetBalance(gomock.Eq(address1)).Return(balance, nil)
+	balance, _ = common.ToBalance(big.NewInt(12))
+	mock.EXPECT().SetBalance(gomock.Eq(address1), gomock.Eq(balance)).Return(nil)
+
+	db.AddBalance(address1, big.NewInt(5))
+	db.SubBalance(address1, big.NewInt(3))
+	db.EndTransaction()
+}
+
+func TestCarmenStateBalanceUnchangedValuesAreNotWritten(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Balance is only read, never written.
+	want := big.NewInt(10)
+	balance, _ := common.ToBalance(want)
+	mock.EXPECT().GetBalance(gomock.Eq(address1)).Return(balance, nil)
+
+	db.AddBalance(address1, big.NewInt(10))
+	db.SubBalance(address1, big.NewInt(5))
+	db.SubBalance(address1, big.NewInt(5))
+	db.EndTransaction()
+}
+
+func TestCarmenStateBalanceIsNotWrittenToStateIfTransactionIsAborted(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Balance is only read, never written.
+	want := big.NewInt(10)
+	balance, _ := common.ToBalance(want)
+	mock.EXPECT().GetBalance(gomock.Eq(address1)).Return(balance, nil)
+
+	db.AddBalance(address1, big.NewInt(10))
+	db.AbortTransaction()
+}
+
+func TestCarmenStateNoncesAreReadFromState(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Set up the expectation that the store will be called once.
+	var want uint64 = 12
+	mock.EXPECT().GetNonce(gomock.Eq(address1)).Return(common.ToNonce(want), nil)
+
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+}
+
+func TestCarmenStateNoncesAreOnlyReadOnce(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Set up the expectation that the store will be called once.
+	var want uint64 = 12
+	mock.EXPECT().GetNonce(gomock.Eq(address1)).Return(common.ToNonce(want), nil)
+
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+	db.GetNonce(address1)
+	db.GetNonce(address1)
+}
+
+func TestCarmenStateNoncesCanBeWrittenAndReadWithoutStateAccess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Mock should never be consulted.
+
+	var want uint64 = 12
+	db.SetNonce(address1, want)
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+	want = 14
+	db.SetNonce(address1, want)
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+}
+
+func TestCarmenStateNoncesCanBeSnapshottedAndReverted(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Nonce is initially 10. This should only be fetched once.
+	mock.EXPECT().GetNonce(gomock.Eq(address1)).Return(common.ToNonce(10), nil)
+
+	snapshot0 := db.Snapshot()
+
+	var want uint64 = 10
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+
+	snapshot1 := db.Snapshot()
+	want = 11
+	db.SetNonce(address1, want)
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+
+	snapshot2 := db.Snapshot()
+	want = 12
+	db.SetNonce(address1, want)
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+
+	db.RevertToSnapshot(snapshot2)
+	want = 11
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+
+	db.RevertToSnapshot(snapshot1)
+	want = 10
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+
+	db.RevertToSnapshot(snapshot0)
+	want = 10
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+}
+
+func TestCarmenStateNoncesOnlySetCanBeReverted(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Nonce is initially 10. This should only be fetched once.
+	mock.EXPECT().GetNonce(gomock.Eq(address1)).Return(common.ToNonce(10), nil)
+
+	snapshot0 := db.Snapshot()
+
+	var want uint64 = 11
+	db.SetNonce(address1, want)
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+
+	db.RevertToSnapshot(snapshot0)
+	want = 10
+	if got := db.GetNonce(address1); got != want {
+		t.Errorf("error retrieving nonce, wanted %v, got %v", want, got)
+	}
+}
+
+func TestCarmenStateNoncesIsWrittenToStateIfChangedAtEndOfTransaction(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// The updated value is expected to be written to the state.
+	mock.EXPECT().SetNonce(gomock.Eq(address1), gomock.Eq(common.ToNonce(10))).Return(nil)
+
+	db.SetNonce(address1, 10)
+	db.EndTransaction()
+}
+
+func TestCarmenStateNoncesOnlyFinalValueIsWrittenAtEndOfTransaction(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Only the last value is to be written to the state.
+	mock.EXPECT().SetNonce(gomock.Eq(address1), gomock.Eq(common.ToNonce(12))).Return(nil)
+
+	db.SetNonce(address1, 10)
+	db.SetNonce(address1, 11)
+	db.SetNonce(address1, 12)
+	db.EndTransaction()
+}
+
+func TestCarmenStateNoncesUnchangedValuesAreNotWritten(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// Nonce is only read, never written.
+	mock.EXPECT().GetNonce(gomock.Eq(address1)).Return(common.ToNonce(10), nil)
+
+	value := db.GetNonce(address1)
+	db.SetNonce(address1, value)
+	db.EndTransaction()
+}
+
+func TestCarmenStateNoncesIsNotWrittenToStateIfTransactionIsAborted(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock := NewMockState(ctrl)
+	db := CreateStateDBUsing(mock)
+
+	// No mock call is expected.
+
+	db.SetNonce(address1, 10)
+	db.AbortTransaction()
 }
 
 func TestCarmenStateValuesAreReadFromState(t *testing.T) {
