8a3aba9502ecdf72f0cb7ade6675d0304be641ff
mike <pelanek.lukas@seznam.cz>
2022-10-19 12:42:06 +0200

    Add more `ToDBKey` leveldb index benchmarks for types

--- cpp/backend/index/leveldb/index_benchmark.cc
+++ cpp/backend/index/leveldb/index_benchmark.cc
@@ -1,5 +1,6 @@
 #include "backend/index/leveldb/index.h"
 #include "benchmark/benchmark.h"
+#include "common/type.h"
 
 namespace carmen::backend::index {
 namespace {
@@ -7,15 +8,42 @@ namespace {
 // To run benchmarks, use the following command:
 //    bazel run -c opt //backend/index/leveldb:index_benchmark
 
-void BM_ToDBKey(benchmark::State& state) {
+void BM_IntToDBKey(benchmark::State& state) {
   for (auto _ : state) {
     auto res = internal::ToDBKey('t', 1);
     benchmark::DoNotOptimize(res);
   }
 }
 
+void BM_BalanceToDBKey(benchmark::State& state) {
+  auto addr = Balance{};
+  for (auto _ : state) {
+    auto res = internal::ToDBKey('t', addr);
+    benchmark::DoNotOptimize(res);
+  }
+}
+
+void BM_AddressToDBKey(benchmark::State& state) {
+  auto addr = Address{};
+  for (auto _ : state) {
+    auto res = internal::ToDBKey('t', addr);
+    benchmark::DoNotOptimize(res);
+  }
+}
+
+void BM_HashToDBKey(benchmark::State& state) {
+  auto addr = Hash{};
+  for (auto _ : state) {
+    auto res = internal::ToDBKey('t', addr);
+    benchmark::DoNotOptimize(res);
+  }
+}
+
 // Register the function as a benchmark
-BENCHMARK(BM_ToDBKey);
+BENCHMARK(BM_IntToDBKey);
+BENCHMARK(BM_BalanceToDBKey);
+BENCHMARK(BM_AddressToDBKey);
+BENCHMARK(BM_HashToDBKey);
 
 }  // namespace
 }  // namespace carmen::backend::index
