e86cd3f309436bbcc9dd2d329a962e63933a1c78
Lukáš Pelánek <8320012+Mike-CZ@users.noreply.github.com>
2022-10-19 13:34:12 +0200

    Add more `ToDBKey` leveldb index benchmarks for types (#101)

--- cpp/backend/index/leveldb/index_benchmark.cc
+++ cpp/backend/index/leveldb/index_benchmark.cc
@@ -1,5 +1,6 @@
 #include "backend/index/leveldb/index.h"
 #include "benchmark/benchmark.h"
+#include "common/type.h"
 
 namespace carmen::backend::index {
 namespace {
@@ -7,15 +8,20 @@ namespace {
 // To run benchmarks, use the following command:
 //    bazel run -c opt //backend/index/leveldb:index_benchmark
 
+template <Trivial Type>
 void BM_ToDBKey(benchmark::State& state) {
+  Type var = Type{};
   for (auto _ : state) {
-    auto res = internal::ToDBKey('t', 1);
+    auto res = internal::ToDBKey('t', var);
     benchmark::DoNotOptimize(res);
   }
 }
 
 // Register the function as a benchmark
-BENCHMARK(BM_ToDBKey);
+BENCHMARK(BM_ToDBKey<int>);
+BENCHMARK(BM_ToDBKey<Balance>);
+BENCHMARK(BM_ToDBKey<Address>);
+BENCHMARK(BM_ToDBKey<Hash>);
 
 }  // namespace
 }  // namespace carmen::backend::index
