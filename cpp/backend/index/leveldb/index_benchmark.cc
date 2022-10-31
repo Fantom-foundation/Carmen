99c3182d980078b7311049efa8a6ed89e4750de7
mike <pelanek.lukas@seznam.cz>
2022-10-31 10:55:51 +0100

    Add leveldb indexes benchmark.

--- cpp/backend/index/leveldb/index_benchmark.cc
+++ cpp/backend/index/leveldb/index_benchmark.cc
@@ -1,9 +1,10 @@
 #include <concepts>
+#include <random>
 
-#include "backend/index/leveldb/single_db/index.h"
 #include "backend/index/leveldb/multi_db/index.h"
-#include "common/file_util.h"
+#include "backend/index/leveldb/single_db/index.h"
 #include "benchmark/benchmark.h"
+#include "common/file_util.h"
 #include "common/type.h"
 
 namespace carmen::backend::index {
@@ -27,11 +28,11 @@ class SingleIndexBM {
   LevelDBKeySpace<K, I>& GetIndex(std::uint8_t index) {
     return indexes_[index];
   }
+
  private:
   TempDir dir_;
   std::vector<LevelDBKeySpace<K, I>> indexes_;
 };
-
 
 template <Trivial K, std::integral I>
 class MultiIndexBM {
@@ -47,6 +48,7 @@ class MultiIndexBM {
   MultiLevelDBIndex<K, I>& GetIndex(std::uint8_t index) {
     return indexes_[index];
   }
+
  private:
   std::vector<TempDir> dirs_;
   std::vector<MultiLevelDBIndex<K, I>> indexes_;
@@ -80,11 +82,9 @@ void BM_Insert(benchmark::State& state) {
 
   auto i = pre_loaded_num_elements;
   for (auto _ : state) {
-    for (std::uint8_t j = 0; j < indexes_count; ++j) {
-      auto& idx = index.GetIndex(j);
-      auto id = idx.GetOrAdd(ToKey(i));
-      benchmark::DoNotOptimize(*id);
-    }
+    auto& idx = index.GetIndex(i % indexes_count);
+    auto id = idx.GetOrAdd(ToKey(i));
+    benchmark::DoNotOptimize(*id);
     ++i;
   }
 }
@@ -121,11 +121,9 @@ void BM_SequentialRead(benchmark::State& state) {
 
   auto i = 0;
   for (auto _ : state) {
-    for (std::uint8_t j = 0; j < indexes_count; ++j) {
-      auto& idx = index.GetIndex(j);
-      auto id = idx.GetOrAdd(ToKey(i));
-      benchmark::DoNotOptimize(*id);
-    }
+    auto& idx = index.GetIndex(i % indexes_count);
+    auto id = idx.GetOrAdd(ToKey(i));
+    benchmark::DoNotOptimize(*id);
     ++i;
   }
 }
@@ -139,6 +137,88 @@ BENCHMARK(BM_SequentialRead<SingleIndex>)
     ->Args({1 << 20, 8});
 
 BENCHMARK(BM_SequentialRead<MultiIndex>)
+    ->Args({1 << 10, 2})
+    ->Args({1 << 20, 2})
+    ->Args({1 << 10, 5})
+    ->Args({1 << 20, 5})
+    ->Args({1 << 10, 8})
+    ->Args({1 << 20, 8});
+
+template <typename LevelDBIndex>
+void BM_UniformRandomRead(benchmark::State& state) {
+  auto pre_loaded_num_elements = state.range(0);
+  auto indexes_count = state.range(1);
+  LevelDBIndex index(indexes_count);
+
+  // Fill in initial elements.
+  for (std::int64_t i = 0; i < pre_loaded_num_elements; i++) {
+    for (std::uint8_t j = 0; j < indexes_count; ++j) {
+      auto& idx = index.GetIndex(j);
+      *idx.GetOrAdd(ToKey(i));
+    }
+  }
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dist(0, pre_loaded_num_elements - 1);
+  for (auto _ : state) {
+    auto i = dist(gen);
+    auto& idx = index.GetIndex(i % indexes_count);
+    auto id = idx.GetOrAdd(ToKey(i));
+    benchmark::DoNotOptimize(*id);
+  }
+}
+
+BENCHMARK(BM_UniformRandomRead<SingleIndex>)
+    ->Args({1 << 10, 2})
+    ->Args({1 << 20, 2})
+    ->Args({1 << 10, 5})
+    ->Args({1 << 20, 5})
+    ->Args({1 << 10, 8})
+    ->Args({1 << 20, 8});
+
+BENCHMARK(BM_UniformRandomRead<MultiIndex>)
+    ->Args({1 << 10, 2})
+    ->Args({1 << 20, 2})
+    ->Args({1 << 10, 5})
+    ->Args({1 << 20, 5})
+    ->Args({1 << 10, 8})
+    ->Args({1 << 20, 8});
+
+template <typename LevelDBIndex>
+void BM_ExponentialRandomRead(benchmark::State& state) {
+  auto pre_loaded_num_elements = state.range(0);
+  auto indexes_count = state.range(1);
+  LevelDBIndex index(indexes_count);
+
+  // Fill in initial elements.
+  for (std::int64_t i = 0; i < pre_loaded_num_elements; i++) {
+    for (std::uint8_t j = 0; j < indexes_count; ++j) {
+      auto& idx = index.GetIndex(j);
+      *idx.GetOrAdd(ToKey(i));
+    }
+  }
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::exponential_distribution<> dist(double(10) / pre_loaded_num_elements);
+  for (auto _ : state) {
+    auto i = dist(gen);
+    auto& idx = index.GetIndex(int(i) % indexes_count);
+    auto id = idx.GetOrAdd(ToKey(i));
+    benchmark::DoNotOptimize(*id);
+  }
+}
+
+BENCHMARK(BM_ExponentialRandomRead<SingleIndex>)
+    ->Args({1 << 10, 2})
+    ->Args({1 << 20, 2})
+    ->Args({1 << 10, 5})
+    ->Args({1 << 20, 5})
+    ->Args({1 << 10, 8})
+    ->Args({1 << 20, 8});
+
+BENCHMARK(BM_ExponentialRandomRead<MultiIndex>)
     ->Args({1 << 10, 2})
     ->Args({1 << 20, 2})
     ->Args({1 << 10, 5})
