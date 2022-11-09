a489911478f6b0866413c5b1b05c322f0d568f70
mike <pelanek.lukas@seznam.cz>
2022-11-09 11:42:40 +0100

    Add static assert

--- cpp/common/byte_util.h
+++ cpp/common/byte_util.h
@@ -7,30 +7,35 @@
 namespace carmen {
 
 // Return a span of bytes representing the given value. Given value has to be
-// trivial.
+// trivially copyable.
 template <typename T>
 std::span<const std::byte> AsBytes(const T& value) {
+  static_assert(std::is_trivially_copyable_v<T>);
   return std::as_bytes(std::span<const T>(&value, 1));
 }
 
 // Return a span of chars representing the given value. Given value has to be
-// trivial.
+// trivially copyable.
 template <typename T>
 std::span<const char> AsChars(const T& value) {
   auto bytes = AsBytes(value);
   return {reinterpret_cast<const char*>(bytes.data()), sizeof(T)};
 }
 
-// Return a value from a span of bytes. Target value has to be trivial.
+// Return a value from a span of bytes. Target value has to be trivially
+// copyable.
 template <typename T>
 absl::StatusOr<T> FromChars(std::span<const char> data) {
+  static_assert(std::is_trivially_copyable_v<T>);
   if (data.size() != sizeof(T)) return absl::InternalError("Invalid data size");
   return *reinterpret_cast<const T*>(data.data());
 }
 
-// Return a value from a span of bytes. Target value has to be trivial.
+// Return a value from a span of bytes. Target value has to be trivially
+// copyable.
 template <typename T>
 absl::StatusOr<T> FromBytes(std::span<const std::byte> data) {
+  static_assert(std::is_trivially_copyable_v<T>);
   if (data.size() != sizeof(T)) return absl::InternalError("Invalid data size");
   return *reinterpret_cast<const T*>(data.data());
 }
