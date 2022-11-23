d1a70b78b252f79ce2fe8927d7a83f2ea7330a41
Herbert Jordan <herbert@swrnd.ch>
2022-11-23 15:52:06 +0100

    Add class to describe memory footprint of object DAG

--- cpp/common/memory_usage.h
+++ cpp/common/memory_usage.h
@@ -1,7 +1,16 @@
 #pragma once
 
 #include <cstdint>
+#include <deque>
 #include <ostream>
+#include <queue>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/functional/function_ref.h"
 
 namespace carmen {
 
@@ -12,35 +21,128 @@ class Memory {
   explicit constexpr Memory(uint64_t bytes = 0) : bytes_(bytes) {}
   auto operator<=>(const Memory&) const = default;
   constexpr std::int64_t bytes() const { return bytes_; }
+
+  Memory& operator+=(const Memory&);
+  Memory& operator-=(const Memory&);
+  Memory& operator*=(int factor);
+
  private:
   std::int64_t bytes_;
 };
 
+template <typename T>
+Memory SizeOf() {
+  return Memory(sizeof(T));
+}
+
+// Approximates the memory usage of the given vector assuming the element type
+// is a stack-only type.
+template <typename T>
+Memory SizeOf(const std::vector<T>& vector) {
+  return SizeOf<T>() * vector.size();
+}
+
+// Approximates the memory usage of the given queue assuming the element type is
+// a stack-only type.
+template <typename T>
+Memory SizeOf(const std::queue<T>& list) {
+  return SizeOf<T>() * list.size();
+}
+
+// Approximates the memory usage of the given deque assuming the element type is
+// a stack-only type.
+template <typename T>
+Memory SizeOf(const std::deque<T>& list) {
+  return SizeOf<T>() * list.size();
+}
+
+// Approximates the memory usage of the given set assuming the element type is a
+// stack-only type.
+template <typename T>
+Memory SizeOf(const absl::flat_hash_set<T>& set) {
+  return SizeOf<T>() * set.size();
+}
+
+// Approximates the memory usage of the given map assuming the key and value
+// types are a stack-only types.
+template <typename K, typename V>
+Memory SizeOf(const absl::flat_hash_map<K, V>& map) {
+  return (SizeOf<K>() + SizeOf<V>()) * map.size();
+}
+
 constexpr Memory operator+(const Memory& a, const Memory& b) {
-return Memory(a.bytes() + b.bytes());
+  return Memory(a.bytes() + b.bytes());
 }
 
 constexpr Memory operator-(const Memory& a, const Memory& b) {
-    return Memory(a.bytes() - b.bytes());
+  return Memory(a.bytes() - b.bytes());
 }
 
 constexpr Memory operator*(const Memory& a, int factor) {
-    return Memory(a.bytes() * factor);
+  return Memory(a.bytes() * factor);
 }
 
-constexpr Memory operator*(int factor, const Memory& a) {
-    return a * factor;
-}
+constexpr Memory operator*(int factor, const Memory& a) { return a * factor; }
 
 std::ostream& operator<<(std::ostream& out, const Memory& memory);
 
 // Some memory constants.
 constexpr static const Memory Byte(1);
-constexpr static const Memory KiB = Byte * 1024;
-constexpr static const Memory MiB = KiB * 1024;
-constexpr static const Memory GiB = MiB * 1024;
-constexpr static const Memory TiB = GiB * 1024;
-constexpr static const Memory PiB = TiB * 1024;
-constexpr static const Memory EiB = PiB * 1024;
+constexpr static const Memory KiB = 1024 * Byte;
+constexpr static const Memory MiB = 1024 * KiB;
+constexpr static const Memory GiB = 1024 * MiB;
+constexpr static const Memory TiB = 1024 * GiB;
+constexpr static const Memory PiB = 1024 * TiB;
+constexpr static const Memory EiB = 1024 * PiB;
+
+// A MemoryFootprint describes the memory usage of a DAG shaped object graph.
+// Each node is the root of a DAG of objects, where each node is a object
+// desribed by a MemoryFootprint instance including its memory usage, and each
+// edge is labeled by a field name.
+class MemoryFootprint {
+ public:
+  using ObjectId = const void*;
+
+  // Initializes a memory footprint description for the given object.
+  template <typename T>
+  MemoryFootprint(const T& obj) : MemoryFootprint(&obj, SizeOf<T>()) {}
+
+  // Creates a new memory footprint object describing the memory usage of a
+  // unique object.
+  MemoryFootprint(Memory self = Memory()) : self_(self) {}
+
+  // Creates a new memory footprint object describing the memory usage of a
+  // potentially shared object. The source pointer is used to identify common
+  // shared objects.
+  MemoryFootprint(ObjectId source, Memory self)
+      : source_(source), self_(self) {}
+
+  // Computes the total memory footprint of the DAG rooted by this node.
+  Memory GetTotal() const;
+
+  // Adds a field with the given footprint to this node.
+  MemoryFootprint& Add(std::string_view label, MemoryFootprint footprint);
+
+  // Prints a summary of the memory described memory usage.
+  friend std::ostream& operator<<(std::ostream& out, const MemoryFootprint&);
+
+ private:
+  void PrintTo(std::ostream& out, std::string_view prefix) const;
+
+  // A unique identifier for shared objects or null, if unique.
+  ObjectId source_ = nullptr;
+
+  // The memory usage of the described object.
+  Memory self_;
+
+  // A list of memory footprints of named sub-components.
+  absl::flat_hash_map<std::string, MemoryFootprint> components_;
+};
+
+// Describes a concept for each type that provides memory footprint information.
+template <typename I>
+concept MemoryFootprintProvider = requires(const I a) {
+  { a.GetMemoryFootprint() } -> std::same_as<MemoryFootprint>;
+};
 
 }  // namespace carmen
