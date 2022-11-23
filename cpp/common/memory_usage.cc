b86f9f821c3e51ff2529ada540a07e7cec03758e
Herbert Jordan <herbert@swrnd.ch>
2022-11-23 15:52:06 +0100

    Add class to describe memory footprint of object DAG

--- cpp/common/memory_usage.cc
+++ cpp/common/memory_usage.cc
@@ -5,7 +5,26 @@
 #include <ostream>
 #include <string_view>
 
+#include "absl/container/flat_hash_set.h"
+#include "absl/functional/function_ref.h"
+#include "absl/strings/str_format.h"
+
 namespace carmen {
+
+Memory& Memory::operator+=(const Memory& a) {
+  bytes_ += a.bytes_;
+  return *this;
+}
+
+Memory& Memory::operator-=(const Memory& a) {
+  bytes_ -= a.bytes_;
+  return *this;
+}
+
+Memory& Memory::operator*=(int factor) {
+  bytes_ *= factor;
+  return *this;
+}
 
 std::ostream& operator<<(std::ostream& out, const Memory& memory) {
   const std::string_view prefixes = " KMGTPE";
@@ -27,8 +46,59 @@ std::ostream& operator<<(std::ostream& out, const Memory& memory) {
     return out << value << " byte";
   }
 
-  return out << std::fixed << std::setprecision(1)
-             << value / std::pow(base, exp) << " " << prefixes[exp] << "iB";
+  return out << absl::StrFormat("%.1f %ciB", value / std::pow(base, exp),
+                                prefixes[exp]);
+}
+
+Memory MemoryFootprint::GetTotal() const {
+  Memory sum;
+  absl::flat_hash_set<ObjectId> seen;
+  std::vector<const MemoryFootprint*> stack;
+  stack.push_back(this);
+  while (!stack.empty()) {
+    const MemoryFootprint& cur = *stack.back();
+    stack.pop_back();
+    sum += cur.self_;
+    // Enqueue sub-components.
+    for (const auto& [_, component] : cur.components_) {
+      // Skip nodes already visited.
+      if (component.source_ != nullptr &&
+          !seen.insert(component.source_).second) {
+        continue;
+      }
+      stack.push_back(&component);
+    }
+  }
+  return sum;
+}
+
+MemoryFootprint& MemoryFootprint::Add(std::string_view label,
+                                      MemoryFootprint footprint) {
+  components_.insert_or_assign(label, std::move(footprint));
+  return *this;
+}
+
+void MemoryFootprint::PrintTo(std::ostream& out, std::string_view path) const {
+  // For improved readability and comparability labels are sorted.
+  std::vector<std::string_view> labels;
+  for (const auto& [label, component] : components_) {
+    labels.push_back(label);
+  }
+  std::sort(labels.begin(), labels.end());
+
+  for (const auto& label : labels) {
+    const auto& component = components_.find(label)->second;
+    component.PrintTo(out,
+                      absl::StrCat(std::string(path), "/", std::string(label)));
+  }
+  std::stringstream buffer;
+  buffer << GetTotal();
+  out << absl::StrFormat("%9s\t%s\n", buffer.str(), path);
+}
+
+std::ostream& operator<<(std::ostream& out, const MemoryFootprint& footprint) {
+  footprint.PrintTo(out, ".");
+  return out;
 }
 
 }  // namespace carmen
--- cpp/common/memory_usage_test.cc
+++ cpp/common/memory_usage_test.cc
@@ -3,10 +3,13 @@
 #include <type_traits>
 
 #include "common/test_util.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace carmen {
 namespace {
+
+using ::testing::HasSubstr;
 
 TEST(MemoryTest, TypeTraits) {
   EXPECT_TRUE(std::is_default_constructible_v<Memory>);
@@ -74,6 +77,87 @@ TEST(MemoryTest, Printing) {
   EXPECT_EQ(Print(4 * TiB), "4.0 TiB");
   EXPECT_EQ(Print(5 * PiB), "5.0 PiB");
   EXPECT_EQ(Print(6 * EiB), "6.0 EiB");
+}
+
+TEST(MemoryFootprintTest, SelfIsIncludedInTotal) {
+  MemoryFootprint a(Memory(12));
+  EXPECT_EQ(a.GetTotal(), Memory(12));
+}
+
+TEST(MemoryFootprintTest, ComponentsAreIncludedInTotal) {
+  MemoryFootprint a;
+  a.Add("B", Memory(10));
+  a.Add("C", Memory(14));
+  EXPECT_EQ(a.GetTotal(), Memory(24));
+}
+
+TEST(MemoryFootprintTest, DeeperHierarchiesAreCovered) {
+  MemoryFootprint l;
+  l.Add("l1", Memory(1));
+  l.Add("l2", Memory(2));
+
+  MemoryFootprint r;
+  r.Add("r1", Memory(3));
+  r.Add("r2", Memory(4));
+  r.Add("r3", Memory(5));
+
+  MemoryFootprint t;
+  t.Add("l", l);
+  t.Add("r", r);
+  EXPECT_EQ(t.GetTotal(), Memory(1 + 2 + 3 + 4 + 5));
+}
+
+TEST(MemoryFootprintTest, CommonSubComponentsAreOnlyCountedOnce) {
+  int obj;
+  MemoryFootprint o(&obj, Memory(10));
+  MemoryFootprint r;
+  r.Add("l", o);
+  r.Add("r", o);
+  EXPECT_EQ(r.GetTotal(), Memory(10));
+}
+
+TEST(MemoryFootprintTest, PrintingListsComponents) {
+  MemoryFootprint l;
+  l.Add("l1", Memory(1));
+  l.Add("l2", Memory(2));
+
+  MemoryFootprint r;
+  r.Add("r1", Memory(3));
+  r.Add("r2", Memory(4));
+  r.Add("r3", Memory(5));
+
+  MemoryFootprint t;
+  t.Add("l", l);
+  t.Add("r", r);
+
+  auto print = Print(t);
+  EXPECT_THAT(print, HasSubstr("1 byte\t./l/l1"));
+  EXPECT_THAT(print, HasSubstr("2 byte\t./l/l2"));
+  EXPECT_THAT(print, HasSubstr("3 byte\t./l"));
+  EXPECT_THAT(print, HasSubstr("3 byte\t./r/r1"));
+  EXPECT_THAT(print, HasSubstr("4 byte\t./r/r2"));
+  EXPECT_THAT(print, HasSubstr("5 byte\t./r/r3"));
+  EXPECT_THAT(print, HasSubstr("12 byte\t./r"));
+  EXPECT_THAT(print, HasSubstr("15 byte\t."));
+}
+
+TEST(MemoryFootprintTest, PrintingListsSharedComponents) {
+  MemoryFootprint s;
+  s.Add("s1", Memory(1));
+  s.Add("s2", Memory(2));
+
+  MemoryFootprint t;
+  t.Add("l", s);
+  t.Add("r", s);
+
+  auto print = Print(t);
+  EXPECT_THAT(print, HasSubstr("1 byte\t./l/s1"));
+  EXPECT_THAT(print, HasSubstr("2 byte\t./l/s2"));
+  EXPECT_THAT(print, HasSubstr("3 byte\t./l"));
+  EXPECT_THAT(print, HasSubstr("1 byte\t./r/s1"));
+  EXPECT_THAT(print, HasSubstr("2 byte\t./r/s2"));
+  EXPECT_THAT(print, HasSubstr("3 byte\t./r"));
+  EXPECT_THAT(print, HasSubstr("3 byte\t."));
 }
 
 }  // namespace
