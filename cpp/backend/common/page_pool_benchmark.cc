ca0508dbddd387ab7576746ac6cab1a5e91b85c4
Herbert Jordan <herbert@swrnd.ch>
2022-11-28 15:31:47 +0100

    tmp - fixes page pool benchmark

--- cpp/backend/common/page_pool_benchmark.cc
+++ cpp/backend/common/page_pool_benchmark.cc
@@ -13,23 +13,39 @@ namespace {
 //    bazel run -c opt //backend/common:page_pool_benchmark
 
 constexpr long kMinPoolSize = 4;
-constexpr long kMaxPoolSize = 1 << 20;
+constexpr long kMaxPoolSize = 1 << 20;   // = 4 GiB page pool (with 4 per KiB page)
+constexpr long kFileSize = 1 << 30;      // = 4 TiB file size (with 4 per KiB page)
 
-using TestPool = PagePool<ArrayPage<int>, InMemoryFile>;
+template<typename Page>
+class DummyFile {
+  public:
+  using page_type = Page;
 
-// Evaluates the performance of signalling read events to policies managing
+  std::size_t GetNumPages() {
+    return kFileSize;
+  }
+
+  void LoadPage(PageId, Page& trg) {}
+  void StorePage(PageId, const Page& src) {}
+  void Flush() {}
+  void Close() {}
+};
+
+using TestPool = PagePool<ArrayPage<int>, DummyFile>;
+
+// Evaluates the performance of reading pages from page pools.
 // pools of different sizes.
 template <typename AccessOrder>
 void BM_ReadTest(benchmark::State& state) {
   auto pool_size = state.range(0);
-  TestPool<Policy> pool(pool_size);
+  TestPool pool(pool_size);
 
   // Warm-up by touching each page once.
   for (int64_t i = 0; i < pool_size; i++) {
     pool.Get(i);
   }
 
-  AccessOrder order(pool_size);
+  AccessOrder order(kFileSize);
   for (auto _ : state) {
     pool.Get(order.Next());
   }
@@ -43,6 +59,37 @@ BENCHMARK(BM_ReadTest<Uniform>)
 
 BENCHMARK(BM_ReadTest<Exponential>)
     ->Range(kMinPoolSize, kMaxPoolSize);
+
+
+// Evaluates the performance of writing to pages in page pools.
+template <typename AccessOrder>
+void BM_WriteTest(benchmark::State& state) {
+  auto pool_size = state.range(0);
+  TestPool pool(pool_size);
+
+  // Warm-up by touching each page once.
+  for (int64_t i = 0; i < pool_size; i++) {
+    pool.Get(i);
+    pool.MarkAsDirty(i);
+  }
+
+  AccessOrder order(kFileSize);
+  for (auto _ : state) {
+    auto pos = order.Next();
+    pool.Get(pos);
+    pool.MarkAsDirty(pos);
+  }
+}
+
+BENCHMARK(BM_WriteTest<Sequential>)
+    ->Range(kMinPoolSize, kMaxPoolSize);
+
+BENCHMARK(BM_WriteTest<Uniform>)
+    ->Range(kMinPoolSize, kMaxPoolSize);
+
+BENCHMARK(BM_WriteTest<Exponential>)
+    ->Range(kMinPoolSize, kMaxPoolSize);
+
 
 }  // namespace
 }  // namespace carmen::backend
