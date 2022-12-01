8285c360cbdd67f90701a04c36bfb3711b56d724
Herbert Jordan <herbert@swrnd.ch>
2022-12-01 11:25:12 +0100

    Add heterogenous map utility

---
+++ cpp/common/heterogenous_map_test.cc
@@ -0,0 +1,86 @@
+#include "common/heterogenous_map.h"
+
+#include <string>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace carmen {
+namespace {
+
+TEST(HeterogenousMapTest, Insert) {
+  HeterogenousMap map;
+  EXPECT_EQ(map.Get<int>(), 0);
+  map.Set<int>(12);
+  EXPECT_THAT(map.Get<int>(), 12);
+}
+
+TEST(HeterogenousMapTest, EntriesCanBeUpdated) {
+  HeterogenousMap map;
+  EXPECT_EQ(map.Get<int>(), 0);
+  map.Set<int>(12);
+  EXPECT_THAT(map.Get<int>(), 12);
+  map.Set<int>(14);
+  EXPECT_THAT(map.Get<int>(), 14);
+}
+
+TEST(HeterogenousMapTest, CanContainMultipleTypes) {
+  HeterogenousMap map;
+  EXPECT_EQ(map.Get<int>(), 0);
+  EXPECT_EQ(map.Get<std::string>(), "");
+
+  map.Set<int>(10);
+  EXPECT_EQ(map.Get<int>(), 10);
+  EXPECT_EQ(map.Get<std::string>(), "");
+
+  map.Set<std::string>("hello");
+  EXPECT_EQ(map.Get<int>(), 10);
+  EXPECT_EQ(map.Get<std::string>(), "hello");
+}
+
+TEST(HeterogenousMapTest, ValuesCanBeRetrievedFromConstantMap) {
+  HeterogenousMap map;
+  const HeterogenousMap& const_map = map;
+
+  EXPECT_EQ(const_map.Get<int>(), 0);
+  map.Set<int>(12);
+  EXPECT_THAT(const_map.Get<int>(), 12);
+  map.Set<int>(14);
+  EXPECT_THAT(const_map.Get<int>(), 14);
+}
+
+TEST(HeterogenousMapTest, RetrievedReferencesAreMutable) {
+  HeterogenousMap map;
+
+  int& value = map.Get<int>();
+  value = 10;
+  EXPECT_EQ(map.Get<int>(), 10);
+  value = 12;
+  EXPECT_EQ(map.Get<int>(), 12);
+}
+
+TEST(HeterogenousMapTest, ContainsDetectsSets) {
+  HeterogenousMap map;
+  EXPECT_FALSE(map.Contains<int>());
+  map.Set<int>(12);
+  EXPECT_TRUE(map.Contains<int>());
+}
+
+TEST(HeterogenousMapTest, ContainsDetectsElementsCreatedByGet) {
+  HeterogenousMap map;
+  EXPECT_FALSE(map.Contains<int>());
+  map.Get<int>();
+  EXPECT_TRUE(map.Contains<int>());
+}
+
+TEST(HeterogenousMapTest, ElementsCanBeRemoved) {
+  HeterogenousMap map;
+  EXPECT_FALSE(map.Contains<int>());
+  map.Get<int>();
+  EXPECT_TRUE(map.Contains<int>());
+  map.Reset<int>();
+  EXPECT_FALSE(map.Contains<int>());
+}
+
+}  // namespace
+}  // namespace carmen
