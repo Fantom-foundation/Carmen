95b72642f42fdc9f3a03c590ee69c881efe1c891
Herbert Jordan <herbert@swrnd.ch>
2023-02-21 12:14:02 +0100

    Add LevelDB archive implementation

---
+++ cpp/archive/leveldb/encoding_test.cc
@@ -0,0 +1,38 @@
+#include "archive/leveldb/encoding.h"
+
+#include <array>
+#include <span>
+
+#include "common/type.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace carmen::archive::leveldb {
+namespace {
+
+using ::testing::ElementsAre;
+
+TEST(Encoding, IntegersAreEncodedInBigEndianFormat) {
+  std::array<char, 4> trg;
+  Write(0x12345678, trg);
+  EXPECT_THAT(trg, ElementsAre(0x12, 0x34, 0x56, 0x78));
+}
+
+TEST(Encoding, EncodedIntegersCanBeDecoded) {
+  std::array<char, 4> trg;
+  for (std::uint32_t i = 0; i < 1000; i++) {
+    Write(i, trg);
+    EXPECT_EQ(ReadUint32(trg), i);
+  }
+}
+
+TEST(Encoding, EncodedTrivialValuesCanBeDecoded) {
+  std::array<char, sizeof(Value)> trg;
+  Value value{1, 2, 3, 4};
+  Write(value, trg);
+  EXPECT_EQ(Read<Value>(std::span<char, sizeof(Value)>(trg)), value);
+  EXPECT_EQ(Read<Value>(std::span<const char, sizeof(Value)>(trg)), value);
+}
+
+}  // namespace
+}  // namespace carmen::archive::leveldb
---
+++ cpp/archive/leveldb/keys.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <array>
+#include <span>
+#include <string_view>
+
+#include "absl/status/statusor.h"
+#include "common/type.h"
+
+namespace carmen::archive::leveldb {
+
+// Prefixes for LevelDB keys to differentiated between different table spaces.
+// These keys are aligned with the Go implementation of the Carmen archive, and
+// should be kept aligned for compatiblity.
+enum class KeyType : char {
+  kBlock = '1',
+  kAccountState = '2',
+  kBalance = '3',
+  kCode = '4',
+  kNonce = '5',
+  kStorage = '6',
+  kAccountHash = '7',
+};
+
+// Provides a label for each key type, or `unknown` for everything else.
+std::string_view ToString(KeyType type);
+
+// To differentiate multiple reincarnations of accounts, reincarnation numbers
+// are utilized in the LevelDB archive. Each time an account is created or
+// deleted, it is increased by 1, starting at 0.
+using ReincarnationNumber = std::uint32_t;
+
+// The key type used for per-block information.
+using BlockKey = std::array<char, 1 + sizeof(BlockId)>;
+
+// Most account properties share a common key format.
+using PropertyKey = std::array<char, 1 + sizeof(Address) + sizeof(BlockId)>;
+using AccountStateKey = PropertyKey;
+using BalanceKey = PropertyKey;
+using CodeKey = PropertyKey;
+using NonceKey = PropertyKey;
+using AccountHashKey = PropertyKey;
+
+// The key to store storage information includes the reincarnation number to
+// suppor efficient state clearing.
+using StorageKey =
+    std::array<char, 1 + sizeof(Address) + sizeof(ReincarnationNumber) +
+                         sizeof(Key) + sizeof(BlockId)>;
+
+// -- Factory functions for storage keys ---
+
+BlockKey GetBlockKey(BlockId block);
+
+AccountStateKey GetAccountStateKey(const Address& address, BlockId block);
+
+AccountHashKey GetAccountHashKey(const Address& address, BlockId block);
+
+BalanceKey GetBalanceKey(const Address& address, BlockId block);
+
+CodeKey GetCodeKey(const Address& address, BlockId block);
+
+NonceKey GetNonceKey(const Address& address, BlockId block);
+
+StorageKey GetStorageKey(const Address& address,
+                         ReincarnationNumber reincarnation, const Key& key,
+                         BlockId block);
+
+// Retrieves the block ID from any type of key. Note: for performance reasons it
+// does not check that the given span encodes a valid key. It only interprets
+// the portion of the provided span that is expected to contain the BlockId.
+BlockId GetBlockFromKey(std::span<const char> key);
+
+// Returns the prefix of the key covering the key space and account.
+std::span<const char> GetAccountPrefix(std::span<const char> key);
+
+// Returns the address encoded in the key. Note: for performance reasons it does
+// not check that the given span encodes a valid key. It merely interprets the
+// protion of the span where an address would be expected.
+const Address& GetAddressFromKey(std::span<const char> data);
+
+// Returns the reincarnation number encoded in a storage key.
+ReincarnationNumber GetReincarnationNumber(const StorageKey& key);
+
+// Returns the slot key encoded in a storage key.
+const Key& GetSlotKey(const StorageKey& key);
+
+}  // namespace carmen::archive::leveldb
