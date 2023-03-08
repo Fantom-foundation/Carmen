3cd17c9ed249493d281a40aeaaa053ba14f91993
Herbert Jordan <herbert@swrnd.ch>
2023-03-08 10:55:25 +0100

    Add interface definitions and example for snapshot infrastructure

---
+++ go/backend/snapshot.go
@@ -0,0 +1,96 @@
+package backend
+
+// Snapshots capture a state of a single or a set of data structures at a
+// moment in time for future reference.
+//
+// Snapshots are volatile, thus they do not survive the current process.
+// Snapshots are created by calling a corresponding factory function, typically
+// `CreateSnapshot` on the data structure to be snapshotted, and released by
+// calling the snapshot's `Release` method.
+//
+// Logically, each snapshot describes a range of `Parts`, where each part
+// describes a chunk of data of the data structure's forzen state. Parts can
+// be retrieved individually, to facilitate streaming of snapshots, potentially
+// even from multiple sources.
+//
+// Each part has a Proof associated, capable of certifying its content. Those
+// proofs are, like Parts, data structure dependent. However, Parts provide can
+// by verified through ther `Verify` method.
+//
+// Furthermore, snapshots provide access to Proofs of Parts without retrieving
+// the full Part and its associated data, for faster cross-validation.
+//
+// Finally, a snapshot combines the proofs of all its parts into a single root
+// proof, to cross-validate snapshots without the need of exchanging all the
+// proofs of its parts. The algorithm used for aggregating those proofs is
+// data structure dependent.
+type Snapshot interface {
+	// GetSize retrieves the number of parts in this snapshot.
+	GetSize() int
+	// GetRootProof retrieves the aggregated proof for this snapshot.
+	GetRootProof() Proof
+	// GetProof retrieves the proof for a part, without loading the part.
+	GetProof(part_number int) (Proof, error)
+	// GetPart retrieves a part of the snapshot.
+	GetPart(part_number int) (Part, error)
+	// VerifyProofs verifies that the proofs of the parts are consistent with
+	// snapshot's root proof. Note: it does not verify individual parts.
+	VerifyProofs() error
+
+	// GetData provides a type-erased view on this snapshot to be used for
+	// syncing data structures, potentially over the network.
+	GetData() SnapshotData
+
+	// Release destroys this snapshot, invalidating all derived objects.
+	Release() error
+}
+
+// Part is a chunk of data of a data structure's snapshot.
+type Part interface {
+	// GetProof returns the proof certifying the content of this part.
+	GetProof() Proof
+	// Verify tests that the owned proof is valid for the contained data.
+	Verify() bool
+	// ToBytes serializes this part such that it can be transfered through IO.
+	ToBytes() []byte
+}
+
+// Proof is a peace of information that can be used to certify the content of
+// a Part or an entire snapshot.
+type Proof interface {
+	// Tests whether this proof is equal to the given proof.
+	Equal(proof Proof) bool
+	// ToBytes serializes this proof such that it can be transfered through IO.
+	ToBytes() []byte
+}
+
+// SnapshotData is a type-erased view on a snapshot that is intended to be used
+// for syncing data between data structure instances.
+type SnapshotData interface {
+	// GetMetaData retrieves snapshot specific metadata describing the content
+	// and structure of the snapshot. For instance, it is likely to include the
+	// snapshot's root proof and number of parts. However, the format is data-
+	// structure specific.
+	GetMetaData() ([]byte, error)
+	// GetProofData retrieves a serialized form of the proof of a requested part.
+	GetProofData(part_number int) ([]byte, error)
+	// GetPartData retrieves a serialized form of a requested part.
+	GetPartData(part_number int) ([]byte, error)
+}
+
+// Snapshotable is an interface to be implemented by data structure to support
+// integration into the snapshotting infrastructure.
+type Snapshotable interface {
+	// GetProof returns a proof a snapshot would exhibit if it would be created
+	// for the current state of the data structure.
+	GetProof() (Proof, error)
+	// CreateSnapshot creates a snapshot of the current state of the data
+	// structure. The snapshot should be shielded from subsequent modifications
+	// and be accessible until released.
+	CreateSnapshot() (Snapshot, error)
+	// SyncTo synchronizes the data structure to the given snapshot data. This
+	// may invalidate any former snapshots created on the data structure. In
+	// particular, it is not required to be able to synchronize to a former
+	// snapshot derived from the targeted data structure.
+	SyncTo(data SnapshotData) error
+}
---
+++ go/backend/snapshot_test.go
@@ -0,0 +1,391 @@
+package backend_test
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"testing"
+
+	"github.com/Fantom-foundation/Carmen/go/backend"
+	"github.com/Fantom-foundation/Carmen/go/common"
+)
+
+// This test file contains an example implementation of a data structure
+// (myDataStructure) supporting snapshots. The snapshot type is mySnapshot,
+// the parts are of type myPart, and the proofs are of type myProof.
+
+// ---------------------------------- Proof -----------------------------------
+
+type myProof struct {
+	hash common.Hash
+}
+
+func (p *myProof) Equal(other backend.Proof) bool {
+	trg := other.(*myProof)
+	return trg != nil && trg.hash == p.hash
+}
+
+func (p *myProof) ToBytes() []byte {
+	return p.hash[:]
+}
+
+func getProofFor(data []byte) myProof {
+	hash := sha256.New()
+	hash.Write(data)
+	var res common.Hash
+	hash.Sum(res[:])
+	return myProof{res}
+}
+
+// ----------------------------------- Part -----------------------------------
+
+type myPart struct {
+	proof *myProof
+	data  []byte
+}
+
+func (p *myPart) GetProof() backend.Proof {
+	return p.proof
+}
+
+func (p *myPart) Verify() bool {
+	have := getProofFor(p.data)
+	return p.GetProof().Equal(&have)
+}
+
+func (p *myPart) ToBytes() []byte {
+	res := p.proof.ToBytes()
+	res = append(res, p.data...)
+	return res
+}
+
+// --------------------------------- Snapshot ---------------------------------
+
+type mySnapshot struct {
+	proof  *myProof
+	size   int
+	source mySnapshotDataSource
+}
+
+func (e *mySnapshot) GetSize() int {
+	return e.size
+}
+
+func (e *mySnapshot) GetRootProof() backend.Proof {
+	return e.proof
+}
+
+func (e *mySnapshot) GetProof(part_number int) (backend.Proof, error) {
+	if part_number >= e.size {
+		return nil, fmt.Errorf("no such part")
+	}
+	return e.source.GetProof(part_number)
+}
+
+func (e *mySnapshot) GetPart(part_number int) (backend.Part, error) {
+	proof, err := e.GetProof(part_number)
+	if err != nil {
+		return nil, err
+	}
+	data, err := e.source.GetData(part_number)
+	if err != nil {
+		return nil, err
+	}
+	return &myPart{proof: proof.(*myProof), data: data}, nil
+}
+
+func (e *mySnapshot) VerifyProofs() error {
+	h := sha256.New()
+	for i := 0; i < e.GetSize(); i++ {
+		proof, err := e.GetProof(i)
+		if err != nil {
+			return err
+		}
+		h.Write(proof.(*myProof).ToBytes())
+	}
+	var hash common.Hash
+	h.Sum(hash[:])
+	if hash != e.proof.hash {
+		return fmt.Errorf("proof validation failed")
+	}
+	return nil
+}
+
+func (e *mySnapshot) GetData() backend.SnapshotData {
+	return &mySnapshotData{e}
+}
+
+func (e *mySnapshot) Release() error {
+	return nil // nothing to do
+}
+
+func createMySnapshotFromData(data backend.SnapshotData) (*mySnapshot, error) {
+	metadata, err := data.GetMetaData()
+	if err != nil {
+		return nil, err
+	}
+	if len(metadata) != common.HashSize+1 {
+		return nil, fmt.Errorf("invalid meta data encoding, invalid number of bytes")
+	}
+	size := int(metadata[0])
+	var hash common.Hash
+	copy(hash[:], metadata[1:])
+	return &mySnapshot{&myProof{hash}, size, &SnapshotDataSource{data}}, nil
+}
+
+type mySnapshotData struct {
+	snapshot *mySnapshot
+}
+
+func (e *mySnapshotData) GetMetaData() ([]byte, error) {
+	res := []byte{}
+	res = append(res, byte(e.snapshot.GetSize()))
+	res = append(res, e.snapshot.proof.hash[:]...)
+	return res, nil
+}
+
+func (e *mySnapshotData) GetProofData(part_number int) ([]byte, error) {
+	proof, err := e.snapshot.GetProof(part_number)
+	if err != nil {
+		return nil, err
+	}
+	return proof.ToBytes(), nil
+}
+
+func (e *mySnapshotData) GetPartData(part_number int) ([]byte, error) {
+	part, err := e.snapshot.GetPart(part_number)
+	if err != nil {
+		return nil, err
+	}
+	return part.ToBytes(), nil
+}
+
+type mySnapshotDataSource interface {
+	GetProof(part_number int) (*myProof, error)
+	GetData(part_number int) ([]byte, error)
+}
+
+type SnapshotDataSource struct {
+	snapshot backend.SnapshotData
+}
+
+func (s *SnapshotDataSource) GetProof(part_number int) (*myProof, error) {
+	data, err := s.snapshot.GetProofData(part_number)
+	if err != nil {
+		return nil, err
+	}
+	if len(data) != common.HashSize {
+		return nil, fmt.Errorf("invalid length of example proof")
+	}
+	var hash common.Hash
+	copy(hash[:], data)
+	return &myProof{hash}, nil
+}
+
+func (s *SnapshotDataSource) GetData(part_number int) ([]byte, error) {
+	data, err := s.snapshot.GetPartData(part_number)
+	if err != nil {
+		return nil, err
+	}
+	if len(data) < common.HashSize {
+		return nil, fmt.Errorf("invalid length of example part")
+	}
+	return data[common.HashSize:], nil
+}
+
+// ------------------------------- DataStructure ------------------------------
+
+type myDataStructure struct {
+	proofs []myProof
+	data   [][]byte
+}
+
+func (e *myDataStructure) Set(pos int, data []byte) {
+	for len(e.data) <= pos {
+		e.data = append(e.data, nil)
+		e.proofs = append(e.proofs, getProofFor(nil))
+	}
+	e.data[pos] = make([]byte, len(data))
+	copy(e.data[pos], data)
+	e.proofs[pos] = getProofFor(data)
+}
+
+func (e *myDataStructure) Get(pos int) []byte {
+	if pos < 0 || pos >= len(e.data) {
+		return nil
+	}
+	return e.data[pos]
+}
+
+func (d *myDataStructure) GetProof() (backend.Proof, error) {
+	h := sha256.New()
+	for _, cur := range d.proofs {
+		h.Write(cur.hash[:])
+	}
+	var hash common.Hash
+	h.Sum(hash[:])
+	return &myProof{hash}, nil
+}
+
+func (d *myDataStructure) CreateSnapshot() (backend.Snapshot, error) {
+	proof, err := d.GetProof()
+	if err != nil {
+		return nil, err
+	}
+	proofs := make([]myProof, len(d.proofs))
+	copy(proofs, d.proofs)
+	data := make([][]byte, len(d.data))
+	for i, cur := range d.data {
+		cpy := make([]byte, len(cur))
+		copy(cpy, cur)
+		data[i] = cpy
+	}
+	return &mySnapshot{proof.(*myProof), len(data), &myDataStructureCopy{proofs, data}}, nil
+}
+
+func (d *myDataStructure) SyncTo(data backend.SnapshotData) error {
+	snapshot, err := createMySnapshotFromData(data)
+	if err != nil {
+		return err
+	}
+
+	d.data = make([][]byte, snapshot.GetSize())
+
+	for i := 0; i < snapshot.GetSize(); i++ {
+		part, err := snapshot.GetPart(i)
+		if err != nil {
+			return err
+		}
+		cpy := make([]byte, len(part.(*myPart).data))
+		copy(cpy, part.(*myPart).data)
+		d.data[i] = cpy
+	}
+	return nil
+}
+
+type myDataStructureCopy struct {
+	proofs []myProof
+	data   [][]byte
+}
+
+func (s *myDataStructureCopy) GetProof(part_number int) (*myProof, error) {
+	if part_number < 0 || part_number >= len(s.proofs) {
+		return nil, fmt.Errorf("no such part")
+	}
+	return &s.proofs[part_number], nil
+}
+
+func (s *myDataStructureCopy) GetData(part_number int) ([]byte, error) {
+	if part_number < 0 || part_number >= len(s.proofs) {
+		return nil, fmt.Errorf("no such part")
+	}
+	return s.data[part_number], nil
+}
+
+// ----------------------------------- Tests ----------------------------------
+
+func TestMyDataStructureImplementsInterfaces(t *testing.T) {
+	var _ backend.Proof = &myProof{}
+	var _ backend.Part = &myPart{}
+	var _ backend.Snapshot = &mySnapshot{}
+	var _ backend.Snapshotable = &myDataStructure{}
+}
+
+func TestSnapshotCanBeCreatedAndRestored(t *testing.T) {
+	original := &myDataStructure{}
+	original.Set(1, []byte{1, 2, 3})
+	original.Set(2, []byte{4, 5})
+	original.Set(3, []byte{7, 8, 9})
+
+	snapshot, err := original.CreateSnapshot()
+	if err != nil {
+		t.Errorf("failed to create snapshot: %v", err)
+		return
+	}
+	if snapshot == nil {
+		t.Errorf("failed to create snapshot")
+		return
+	}
+
+	// clear the original data structure, to eliminate the old copy.
+	original.data = [][]byte{}
+
+	recovered := &myDataStructure{}
+	if err := recovered.SyncTo(snapshot.GetData()); err != nil {
+		t.Errorf("failed to sync to snapshot: %v", err)
+		return
+	}
+
+	common.AssertArraysEqual(t, recovered.Get(1), []byte{1, 2, 3})
+	common.AssertArraysEqual(t, recovered.Get(2), []byte{4, 5})
+	common.AssertArraysEqual(t, recovered.Get(3), []byte{7, 8, 9})
+
+	if err := snapshot.Release(); err != nil {
+		t.Errorf("failed to release snapshot: %v", err)
+	}
+}
+
+func TestSnapshotCanBeCreatedAndValidated(t *testing.T) {
+	structure := &myDataStructure{}
+	structure.Set(1, []byte{1, 2, 3})
+	structure.Set(2, []byte{4, 5})
+	structure.Set(3, []byte{7, 8, 9})
+
+	snapshot, err := structure.CreateSnapshot()
+	if err != nil {
+		t.Errorf("failed to create snapshot: %v", err)
+		return
+	}
+	if snapshot == nil {
+		t.Errorf("failed to create snapshot")
+		return
+	}
+
+	remote, err := createMySnapshotFromData(snapshot.GetData())
+	if err != nil {
+		t.Fatalf("failed to create snapshot from snapshot data: %v", err)
+	}
+
+	// Test direct and serialized snapshot data access.
+	for _, cur := range []backend.Snapshot{snapshot, remote} {
+
+		// The root proof should be equivalent.
+		want, err := structure.GetProof()
+		if err != nil {
+			t.Errorf("failed to get root proof from data structure")
+		}
+
+		have := cur.GetRootProof()
+		if !want.Equal(have) {
+			t.Errorf("root proof of snapshot does not match proof of data structure")
+		}
+
+		if err := cur.VerifyProofs(); err != nil {
+			t.Errorf("snapshot invalid, inconsistent proofs")
+		}
+
+		// Verify all pages
+		for i := 0; i < cur.GetSize(); i++ {
+			want, err := cur.GetProof(i)
+			if err != nil {
+				t.Errorf("failed to fetch proof of part %d", i)
+			}
+			part, err := cur.GetPart(i)
+			if err != nil {
+				t.Errorf("failed to fetch part %d", i)
+			}
+			if !want.Equal(part.GetProof()) {
+				t.Errorf("proof of part does not equal proof provided by snapshot")
+			}
+			if !part.Verify() {
+				t.Errorf("failed to verify content of part %d", i)
+			}
+		}
+	}
+
+	if err := remote.Release(); err != nil {
+		t.Errorf("failed to release remote snapshot: %v", err)
+	}
+	if err := snapshot.Release(); err != nil {
+		t.Errorf("failed to release snapshot: %v", err)
+	}
+}
