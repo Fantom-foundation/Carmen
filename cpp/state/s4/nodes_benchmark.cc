3062726cc0deab2372a8d3f20ecfcd4d00a61ca4
Herbert Jordan <herbert@swrnd.ch>
2023-05-04 13:26:44 +0200

    Add benchmark for MPT insertion

---
+++ cpp/state/s4/nodes_benchmark.cc
@@ -0,0 +1,23 @@
+#include "state/s4/nodes.h"
+#include "benchmark/benchmark.h"
+
+namespace carmen::s4 {
+namespace {
+
+// To run benchmarks, use the following command:
+//    bazel run -c opt //state/s4:nodes_benchmark
+
+// Benchmark the hashing of a sequence of bytes.
+void BM_InsertionSpeed(benchmark::State& state) {
+  auto num_elements = state.range(0);
+  MerklePatriciaTrie<std::uint64_t, int> trie;
+  int next = 0;
+  for (auto _ : state) {
+    trie.Set((next % num_elements)<<10, next);
+  }
+}
+
+BENCHMARK(BM_InsertionSpeed)->Range(1, 1 << 21);
+
+}  // namespace
+}  // namespace carmen::backend::index
