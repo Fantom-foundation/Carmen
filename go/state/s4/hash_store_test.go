52c238ea6aeac0d9dbcdafbc7c3dfac5875bcd5f
Herbert Jordan <herbert@swrnd.ch>
2023-07-25 16:49:07 +0200

    Add hashing and forest infrastructure

---
+++ go/state/s4/hash_store_test.go
@@ -0,0 +1,51 @@
+package s4
+
+import (
+	"testing"
+
+	"github.com/Fantom-foundation/Carmen/go/common"
+)
+
+func TestInMemoryHashStore_SetAndGet(t *testing.T) {
+	const N = 1000
+	store, err := OpenInMemoryHashStore(t.TempDir())
+	if err != nil {
+		t.Fatalf("failed to open store: %v", err)
+	}
+	defer store.Close()
+
+	for i := 0; i < 2*N; i++ {
+		if err := store.Set(ValueId(uint64(i)), common.Hash{byte(i >> 16), byte(i >> 8), byte(i)}); err != nil {
+			t.Fatalf("failed to set hash: %v", err)
+		}
+	}
+
+	for i := 0; i < 2*N; i++ {
+		want := common.Hash{byte(i >> 16), byte(i >> 8), byte(i)}
+		if hash, err := store.Get(ValueId(uint64(i))); err != nil || hash != want {
+			t.Fatalf("fetched invalid hash for %d, got %v, wanted %v, err %v", i, hash, want, err)
+		}
+	}
+}
+
+func TestFileBasedHashStore_SetAndGet(t *testing.T) {
+	const N = 1000
+	store, err := openFileBasedHashStore(t.TempDir(), N)
+	if err != nil {
+		t.Fatalf("failed to open store: %v", err)
+	}
+	defer store.Close()
+
+	for i := 0; i < 2*N; i++ {
+		if err := store.Set(ValueId(uint64(i)), common.Hash{byte(i >> 16), byte(i >> 8), byte(i)}); err != nil {
+			t.Fatalf("failed to set hash: %v", err)
+		}
+	}
+
+	for i := 0; i < 2*N; i++ {
+		want := common.Hash{byte(i >> 16), byte(i >> 8), byte(i)}
+		if hash, err := store.Get(ValueId(uint64(i))); err != nil || hash != want {
+			t.Fatalf("fetched invalid hash for %d, got %v, wanted %v, err %v", i, hash, want, err)
+		}
+	}
+}
