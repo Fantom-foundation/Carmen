0d98624d4bef3f7efe463cf9bbe1ee3cb4817044
Herbert Jordan <herbert@swrnd.ch>
2023-08-24 10:47:01 +0200

    Add sync-wrapper for stock implementations

---
+++ go/backend/stock/synced/synced.go
@@ -0,0 +1,64 @@
+package synced
+
+import (
+	"sync"
+
+	"github.com/Fantom-foundation/Carmen/go/backend/stock"
+	"github.com/Fantom-foundation/Carmen/go/common"
+)
+
+type syncedStock[I stock.Index, V any] struct {
+	mu     sync.Mutex
+	nested stock.Stock[I, V]
+}
+
+// Sync wraps the provided stock into a synchronizing wrapper making sure that
+// at any time only one operation is performed on the given stock.
+func Sync[I stock.Index, V any](stock stock.Stock[I, V]) stock.Stock[I, V] {
+	if res, ok := stock.(*syncedStock[I, V]); ok {
+		return res
+	}
+	return &syncedStock[I, V]{nested: stock}
+}
+
+func (s *syncedStock[I, V]) New() (I, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.nested.New()
+}
+
+func (s *syncedStock[I, V]) Get(index I) (V, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.nested.Get(index)
+}
+
+func (s *syncedStock[I, V]) Set(index I, value V) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.nested.Set(index, value)
+}
+
+func (s *syncedStock[I, V]) Delete(index I) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.nested.Delete(index)
+}
+
+func (s *syncedStock[I, V]) GetMemoryFootprint() *common.MemoryFootprint {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.nested.GetMemoryFootprint()
+}
+
+func (s *syncedStock[I, V]) Flush() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.nested.Flush()
+}
+
+func (s *syncedStock[I, V]) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.nested.Close()
+}
