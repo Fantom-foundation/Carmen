cd66ce8278b0ff052afcdb15b2e68a617d5d9d69
Kamil Jezek <kamil.jezek@gmail.com>
2023-08-30 15:21:56 +0200

    uncomment old benchmark

--- go/common/nways_cache_benchmark_test.go
+++ go/common/nways_cache_benchmark_test.go
@@ -1,23 +1,24 @@
 package common
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 )
 
 var isink int
 
-//func BenchmarkNWaysEvictions(b *testing.B) {
-//	cacheSize := 100_000
-//	for ways := 2; ways < 128; ways *= 2 {
-//		c := NewNWaysCache[int, int](cacheSize, ways)
-//		b.Run(fmt.Sprintf("N-ways cache N %d", ways), func(b *testing.B) {
-//			for i := 0; i < b.N; i++ {
-//				c.Set(i, i)
-//			}
-//		})
-//	}
-//}
+func BenchmarkNWaysEvictions(b *testing.B) {
+	cacheSize := 100_000
+	for ways := 2; ways < 128; ways *= 2 {
+		c := NewNWaysCache[int, int](cacheSize, ways)
+		b.Run(fmt.Sprintf("N-ways cache N %d", ways), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				c.Set(i, i)
+			}
+		})
+	}
+}
 
 func BenchmarkNWaysSequentialWrites(b *testing.B) {
 	b.StopTimer()
