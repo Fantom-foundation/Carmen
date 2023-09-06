814ac9b706e0ef052967a15f25079f856ef1a530
Jan Kalina <jan.kalina@fantom.foundation>
2023-09-06 09:55:36 +0200

    Fix by review

--- go/state/go_state.go
+++ go/state/go_state.go
@@ -209,6 +209,9 @@ func (s *GoState) GetArchiveState(block uint64) (as State, err error) {
 }
 
 func (s *GoState) GetLastArchiveBlockHeight() (uint64, error) {
+	if s.archive == nil {
+		return 0, fmt.Errorf("archive not enabled for this GoState")
+	}
 	lastBlock, err := s.archive.GetLastBlockHeight()
 	if err != nil {
 		return 0, fmt.Errorf("failed to get last block in the archive; %s", err)
