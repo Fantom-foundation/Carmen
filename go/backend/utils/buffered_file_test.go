26a7d8faafd3e061b9cad7587652d1e655b790da
Kamil Jezek <kamil.jezek@gmail.com>
2023-10-16 13:04:32 +0200

    introduces fuzzing campaign for buffered files

--- go/backend/utils/buffered_file_test.go
+++ go/backend/utils/buffered_file_test.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -138,4 +140,110 @@ func TestBufferedFile_WriteAndReadAddBufferBoundary(t *testing.T) {
 	if !bytes.Equal(src, dst) {
 		t.Errorf("failed to read data written across buffer boundary, wanted %v, got %v", src, dst)
 	}
+}
+
+// updatePair consists of a position and data to use in fuzzing of the buffered file.
+// This pair is used either to write and read data at certain position in the file.
+// Depending of the use, the data payload either contains data to write to the while
+// or data expected to be read from the file.
+type updatePair struct {
+	pos  int64
+	data []byte
+}
+
+// serialise converts the updatePair to a byte array.
+// The format is simple: <position><len><data>
+func (p *updatePair) serialise() []byte {
+	res := make([]byte, 0, len(p.data)+8+2)
+	res = binary.BigEndian.AppendUint64(res, uint64(p.pos))
+	res = binary.BigEndian.AppendUint16(res, uint16(len(p.data)))
+	return append(res, p.data...)
+}
+
+// parseUpdates convert input bytes in the array of updatePair.
+// The format is a set of tuples: <position><len><data>
+// This method tries to parse as many those tuples as possible, terminating when no more
+// elements are available.
+// Certain modifications of input sequence may take place:
+// first the position is caped to 2bytes not to seek in extensively huge files
+// secondly, the data payload is trimmed to max 2 * bufferSize.
+func parseUpdates(b []byte) []updatePair {
+	var pairs []updatePair
+	for len(b) > 4 {
+		// cap position to decent numbers not to allocate huge files
+		pos := binary.BigEndian.Uint16(b[0:2])
+		// cap the length to max buffer because
+		// a) payloads spanning more buffers is not supported
+		// b) slices for full address space of Uint16 cannot be created
+		l := binary.BigEndian.Uint16(b[2:4]) % (2 * bufferSize)
+		if len(b) < int(l)+4 {
+			l = uint16(len(b) - 4)
+		}
+		data := b[4 : l+4]
+		pairs = append(pairs, updatePair{int64(pos), data})
+		b = b[l+4:]
+	}
+	return pairs
+}
+
+func FuzzBufferedFile_ReadWrite(f *testing.F) {
+	length := 5 * bufferSize // length above the buffer size
+	long := make([]byte, length)
+	for i := 0; i < length; i++ {
+		long[i]++
+	}
+
+	updates := []updatePair{
+		{0, []byte("Hello")},
+		{2, []byte("A")},
+		{5, []byte("aaaaaaaaaaaaaaaaa")},
+		{10, []byte{}},
+		{20, long},
+		{50, []byte("123456")},
+		{int64(length), []byte("Exceeds one buffer")},
+	}
+
+	// sample variants of the updates to seed the fuzzing
+	for start := 0; start < len(updates); start++ {
+		for end := start; end < len(updates); end++ {
+			var raw []byte
+			for _, update := range updates[start:end] {
+				raw = append(raw, update.serialise()...)
+			}
+			f.Add(raw)
+		}
+	}
+
+	f.Fuzz(func(t *testing.T, rawData []byte) {
+		path := t.TempDir() + "/test.dat"
+		file, err := OpenBufferedFile(path)
+		if err != nil {
+			t.Fatalf("failed to open buffered file: %v", err)
+		}
+		defer file.Close()
+
+		ops := parseUpdates(rawData)
+		for _, op := range ops {
+			if err := file.Write(op.pos, op.data); err != nil {
+				// expected errors in certain situations
+				if len(op.data) >= bufferSize && strings.HasPrefix(err.Error(), "writing data >") {
+					continue
+				}
+				if op.pos < 0 && strings.HasPrefix(err.Error(), "cannot write at negative position") {
+					continue
+				}
+
+				t.Errorf("error to write to file: %s", err)
+			}
+
+			dst := make([]byte, len(op.data))
+			if err := file.Read(op.pos, dst); err != nil {
+				t.Fatalf("error to read from file: %s", err)
+			}
+
+			if !bytes.Equal(op.data, dst) {
+				t.Errorf("data read from file does not match written data: %x != %x", op.data, dst)
+			}
+		}
+	})
 }
