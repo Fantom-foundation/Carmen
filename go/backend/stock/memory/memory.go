5443f719378fc3e3240b2262d37b8ee06c74a98b
Kamil Jezek <kamil.jezek@gmail.com>
2023-10-24 09:00:20 +0200

    removes emptying value from the memory stock when the ID is released (#591)

--- go/backend/stock/memory/memory.go
+++ go/backend/stock/memory/memory.go
@@ -128,8 +128,6 @@ func (s *inMemoryStock[I, V]) New() (I, error) {
 	if lenFreeList > 0 {
 		index = s.freeList[lenFreeList-1]
 		s.freeList = s.freeList[0 : lenFreeList-1]
-		var value V
-		s.values[index] = value
 	} else {
 		var value V
 		s.values = append(s.values, value)
