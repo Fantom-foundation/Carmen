09c33b4fd86710ef352dd6ffa1dffed0294baf94
Kamil Jezek <kamil.jezek@gmail.com>
2023-10-30 10:33:44 +0100

    integrates fuzzing of Stock implementations with the fuzzing framework (#595)

--- go/backend/stock/stock_fuzzing_utils.go
+++ go/backend/stock/stock_fuzzing_utils.go
@@ -2,15 +2,26 @@ package stock
 
 import (
 	"encoding/binary"
+	"github.com/Fantom-foundation/Carmen/go/fuzzing"
 	"strings"
 	"testing"
 )
+
+func FuzzStockRandomOps(f *testing.F, factory OpenStockFactory, shouldClose bool) {
+	fuzzing.Fuzz[stockFuzzContext](f, &stockFuzzCampaign{shouldClose, factory})
+}
 
 // OpenStockFactory is a factory to open a Stock.
 type OpenStockFactory func(directory string) (Stock[int, int], error)
 
 // opType is operation type to be applied to a stock.
 type opType byte
+
+func (o opType) serialize() []byte {
+	b := make([]byte, 1, 7) // one byte for type + uint16 for position + uint32 for value
+	b[0] = byte(o)
+	return b
+}
 
 const (
 	newId opType = iota
@@ -20,27 +31,167 @@ const (
 	getIds
 )
 
-// op is a tuple of opType, index in the stock, and data.
-type op struct {
-	opType
+type opSet struct {
 	index int
 	value int
 }
 
-// serialise converts the struct to a byte array
-// using following format: <opType><index><value>
-// The index is exported only for operations get, set and deleteId
-// and the data is exported only for operation set.
-func (o *op) serialise() []byte {
-	b := []byte{byte(o.opType)}
-	if o.opType == get || o.opType == set || o.opType == deleteId {
-		b = binary.BigEndian.AppendUint16(b, uint16(o.index))
+func (op *opSet) Serialize() []byte {
+	b := set.serialize()
+	b = binary.BigEndian.AppendUint16(b, uint16(op.index))
+	b = binary.BigEndian.AppendUint32(b, uint32(op.value))
+	return b
+}
+
+func (op *opSet) Apply(t *testing.T, c *stockFuzzContext) {
+	if err := c.stock.Set(op.index, op.value); err != nil {
+		_, wasGenerated := c.ids[op.index]
+		if !wasGenerated && strings.HasPrefix(err.Error(), "index out of range") {
+			// OK state - cannot set at index that was not created before
+			return
+		}
+		t.Errorf("cannot set: %d -> %d, err: %s", op.index, op.value, err)
 	}
-	if o.opType == set {
-		b = binary.BigEndian.AppendUint32(b, uint32(o.value))
+	// insert into shadow map only when the ID was generated
+	if _, exists := c.ids[op.index]; exists {
+		c.values[op.index] = op.value
+	}
+}
+
+type opGet struct {
+	index int
+}
+
+func (op *opGet) Serialize() []byte {
+	return binary.BigEndian.AppendUint16(get.serialize(), uint16(op.index))
+}
+
+func (op *opGet) Apply(t *testing.T, c *stockFuzzContext) {
+	val, err := c.stock.Get(op.index)
+	if err != nil {
+		t.Errorf("cannot get: %d -> %d, err: %s", op.index, val, err)
+	}
+	if want, exists := c.values[op.index]; exists && want != val {
+		t.Errorf("value set before does not match returned value: %d != %d", val, c.values[op.index])
+	}
+}
+
+type opDeleteId struct {
+	index int
+}
+
+func (op *opDeleteId) Serialize() []byte {
+	return binary.BigEndian.AppendUint16(deleteId.serialize(), uint16(op.index))
+}
+
+func (op *opDeleteId) Apply(t *testing.T, c *stockFuzzContext) {
+	// allow for deleting only IDs that were generated
+	// - this is on purpose not checked in stock implementation.
+	if _, exists := c.ids[op.index]; exists {
+		if err := c.stock.Delete(op.index); err != nil {
+			t.Errorf("error to delete index: %s", err)
+		}
+		delete(c.ids, op.index)
+		delete(c.values, op.index)
+	}
+}
+
+type opGetIds struct {
+}
+
+func (op *opGetIds) Serialize() []byte {
+	return getIds.serialize()
+}
+
+func (op *opGetIds) Apply(t *testing.T, c *stockFuzzContext) {
+	set, err := c.stock.GetIds()
+	if err != nil {
+		t.Errorf("cannot get Ids: %s", err)
+	}
+	for id := range c.ids {
+		if !set.Contains(id) {
+			t.Errorf("set does not contain ID: %d, generated: %v", id, c.ids)
+		}
+	}
+}
+
+type opNewId struct {
+}
+
+func (op *opNewId) Serialize() []byte {
+	return newId.serialize()
+}
+
+func (op *opNewId) Apply(t *testing.T, c *stockFuzzContext) {
+	id, err := c.stock.New()
+	if err != nil {
+		t.Errorf("error to generate new ID: %s", err)
+	}
+	// mark the id was generated, and check it has not been yet used
+	_, exists := c.ids[id]
+	if exists {
+		t.Errorf("Stock generated ID that was already genereated and not released: %d", id)
+	}
+	c.ids[id] = true
+}
+
+// stockFuzzContext carries the stock under test and shadow list of generated IDs
+// and values.
+type stockFuzzContext struct {
+	stock  Stock[int, int]
+	ids    map[int]bool
+	values map[int]int
+}
+
+// stockFuzzCampaign initialises fuzzing campaign of Stock implementations.
+type stockFuzzCampaign struct {
+	shouldClose bool
+	factory     OpenStockFactory
+}
+
+func (c *stockFuzzCampaign) Init() []fuzzing.OperationSequence[stockFuzzContext] {
+	payload1 := 99
+	payload2 := ^99
+
+	// generate some adhoc sequences of operations
+	data := []fuzzing.OperationSequence[stockFuzzContext]{
+		{&opNewId{}, &opSet{0, payload1}, &opGet{0}, &opDeleteId{0}},
+		{&opNewId{}, &opNewId{}, &opSet{0, payload1}, &opSet{1, payload2},
+			&opGet{0}, &opGet{1}, &opDeleteId{0}, &opDeleteId{1}},
+		{&opNewId{}, &opNewId{}, &opDeleteId{0}, &opDeleteId{1}},
+		{&opNewId{}, &opNewId{}, &opGetIds{}},
+		{&opNewId{}, &opNewId{}, &opSet{1, payload2}, &opGet{1}},
+		{&opNewId{}, &opDeleteId{0}, &opNewId{}, &opSet{0, payload1}, &opGet{0}},
+		{&opGet{0}, &opSet{0, payload1}, &opDeleteId{0}},
+		{&opDeleteId{0}, &opGet{0}, &opSet{0, payload1}, &opNewId{}},
 	}
 
-	return b
+	return data
+}
+
+func (c *stockFuzzCampaign) CreateContext(t *testing.T) *stockFuzzContext {
+	path := t.TempDir() + "/test.dat"
+	st, err := c.factory(path)
+	if err != nil {
+		t.Fatalf("failed to open buffered file: %v", err)
+	}
+
+	ids := make(map[int]bool)
+	values := make(map[int]int)
+
+	return &stockFuzzContext{st, ids, values}
+}
+
+func (c *stockFuzzCampaign) Deserialize(rawData []byte) []fuzzing.Operation[stockFuzzContext] {
+	return parseOperations(rawData)
+}
+
+func (c *stockFuzzCampaign) Cleanup(t *testing.T, context *stockFuzzContext) {
+	if c.shouldClose {
+		if err := context.stock.Close(); err != nil {
+			t.Fatalf("cannot close file: %s", err)
+		}
+	}
 }
 
 // parseOperations converts the input byte array
@@ -56,8 +207,8 @@ func (o *op) serialise() []byte {
 // number of these operations to 20 in total and 3 in row.
 // The fuzzing mechanism requires one campaign does not run more than 1s, which is quickly
 // broken when an expensive operation is triggered extensively.
-func parseOperations(b []byte) []op {
-	var ops []op
+func parseOperations(b []byte) []fuzzing.Operation[stockFuzzContext] {
+	var ops []fuzzing.Operation[stockFuzzContext]
 	var expensiveOpTotal, expensiveOpRow int
 	for len(b) >= 1 {
 		opType := opType(b[0] % 5)
@@ -91,110 +242,21 @@ func parseOperations(b []byte) []op {
 			}
 		}
 
-		ops = append(ops, op{opType, index, value})
+		var op fuzzing.Operation[stockFuzzContext]
+		switch opType {
+		case get:
+			op = &opGet{index}
+		case set:
+			op = &opSet{index, value}
+		case deleteId:
+			op = &opDeleteId{index}
+		case newId:
+			op = &opNewId{}
+		case getIds:
+			op = &opGetIds{}
+		}
+
+		ops = append(ops, op)
 	}
 	return ops
-}
-
-func FuzzStock_RandomOps(f *testing.F, factory OpenStockFactory, shouldClose bool) {
-
-	payload1 := 99
-	payload2 := ^99
-	empty := 0
-
-	// generate some adhoc sequences of operations
-	data := [][]op{
-		{op{newId, 0, empty}, op{set, 0, payload1}, op{get, 0, empty}, op{deleteId, 0, empty}},
-		{op{newId, 0, empty}, op{newId, 0, empty}, op{set, 0, payload1}, op{set, 1, payload2},
-			op{get, 0, empty}, op{get, 1, empty}, op{deleteId, 0, empty}, op{deleteId, 1, empty}},
-		{op{newId, 0, empty}, op{newId, 0, empty}, op{deleteId, 0, empty}, op{deleteId, 1, empty}},
-		{op{newId, 0, empty}, op{newId, 0, empty}, op{getIds, 0, empty}},
-		{op{newId, 0, empty}, op{newId, 0, empty}, op{set, 1, payload2}, op{get, 1, empty}},
-		{op{newId, 0, empty}, op{deleteId, 0, empty}, op{newId, 0, empty}, op{set, 0, payload1}, op{get, 0, empty}},
-		{op{get, 0, empty}, op{set, 0, payload1}, op{deleteId, 0, empty}},
-		{op{deleteId, 0, empty}, op{get, 0, empty}, op{set, 0, payload1}, op{newId, 0, empty}},
-	}
-
-	for _, line := range data {
-		var raw []byte
-		for _, op := range line {
-			raw = append(raw, op.serialise()...)
-		}
-		f.Add(raw)
-	}
-
-	f.Fuzz(func(t *testing.T, rawData []byte) {
-		path := t.TempDir() + "/test.dat"
-		st, err := factory(path)
-		if err != nil {
-			t.Fatalf("failed to open buffered file: %v", err)
-		}
-		if shouldClose {
-			defer st.Close()
-		}
-
-		ids := make(map[int]bool)
-		values := make(map[int]int)
-
-		ops := parseOperations(rawData)
-		for _, op := range ops {
-			switch op.opType {
-			case newId:
-				id, err := st.New()
-				if err != nil {
-					t.Errorf("error to generate new ID: %s", err)
-				}
-				// mark the id was generated, and check it has not been yet used
-				_, exists := ids[id]
-				if exists {
-					t.Errorf("Stock generated ID that was already genereated and not released: %d", id)
-				}
-				ids[id] = true
-			case set:
-				if err := st.Set(op.index, op.value); err != nil {
-					_, wasGenerated := ids[op.index]
-					if !wasGenerated && strings.HasPrefix(err.Error(), "index out of range") {
-						// OK state - cannot set at index that was not created before
-						continue
-					}
-					t.Errorf("cannot set: %d -> %d, err: %s", op.index, op.value, err)
-				}
-				// insert into shadow map only when the ID was generated
-				if _, exists := ids[op.index]; exists {
-					values[op.index] = op.value
-				}
-			case get:
-				val, err := st.Get(op.index)
-				if err != nil {
-					t.Errorf("cannot get: %d -> %d, err: %s", op.index, op.value, err)
-				}
-				if want, exists := values[op.index]; exists && want != val {
-					t.Errorf("value set before does not match returned value: %d != %d", val, values[op.index])
-				}
-			case deleteId:
-				// allow for deleting only IDs that were generated
-				// - this is on purpose not checked in stock implementation.
-				if _, exists := ids[op.index]; exists {
-					if err := st.Delete(op.index); err != nil {
-						t.Errorf("error to delete index: %s", err)
-					}
-					delete(ids, op.index)
-					delete(values, op.index)
-				}
-			case getIds:
-				set, err := st.GetIds()
-				if err != nil {
-					t.Errorf("cannot get Ids: %s", err)
-				}
-				for id := range ids {
-					if !set.Contains(id) {
-						t.Errorf("set does not contain ID: %d, generated: %v", id, ids)
-					}
-				}
-			default:
-				t.Fatalf("unknown op: %v", op.opType)
-			}
-		}
-
-	})
 }
