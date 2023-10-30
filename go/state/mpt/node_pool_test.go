10a0507a3a8794c48c64436d4bc4cdde038857c3
Herbert Jordan <herbert@swrnd.ch>
2023-10-30 16:35:00 +0100

    Replace the node cache with a specialized node pool

---
+++ go/state/mpt/node_pool_test.go
@@ -0,0 +1,63 @@
+package mpt
+
+import (
+	"testing"
+
+	"go.uber.org/mock/gomock"
+)
+
+func TestNodePool_ReferenceLifeCycle(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	source := NewMockNodePoolSource(ctrl)
+
+	pool := NewNodePool(10, source)
+
+	id := ValueId(12)
+	ref := NewNodeReference(id)
+
+	source.EXPECT().getSharedNode(id).Return(EmptyNode{}, nil)
+
+	if want, got := id, ref.Id(); want != got {
+		t.Errorf("unexpected ID, wanted %v, got %v", want, got)
+	}
+
+	ref = NewNodeReference(id)
+	if want, got := id, ref.Id(); want != got {
+		t.Errorf("unexpected ID, wanted %v, got %v", want, got)
+	}
+
+	read, err := ref.GetReadAccess(pool)
+	if err != nil {
+		t.Errorf("failed to get read access: %v", err)
+	}
+	read.Release()
+}
+
+func TestNodePool_CapacityIsEnforced(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	source := NewMockNodePoolSource(ctrl)
+
+	source.EXPECT().getSharedNode(gomock.Any()).Times(100).Return(EmptyNode{}, nil)
+
+	pool := NewNodePool(10, source)
+
+	for i := 0; i < 100; i++ {
+		id := ValueId(uint64(i))
+		ref := NewNodeReference(id)
+		read, err := ref.GetReadAccess(pool)
+		if err != nil {
+			t.Errorf("failed to get read access: %v", err)
+		}
+		read.Release()
+
+		size := pool.Size()
+		if size > 10 {
+			t.Fatalf("pool grow beyond capacity: got %d, capacity %d", size, 10)
+		}
+
+		indexSize := len(pool.(*nodePool).index)
+		if indexSize != size {
+			t.Fatalf("pool size and index size do not match, pool size is %d and index size is %d", size, indexSize)
+		}
+	}
+}
