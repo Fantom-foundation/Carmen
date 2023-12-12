7895af67476ecd8bb1463f263d8ba1f022111b20
Kamil Jezek <kamil.jezek@gmail.com>
2023-12-12 19:02:51 +0100

    use 60% of memory

--- go/state/mpt/verification.go
+++ go/state/mpt/verification.go
@@ -390,7 +390,7 @@ func loadNodeHashes(
 // It is computed as 80% of the main memory divided by the input item size.
 func getBatchSize(itemSize uint) uint64 {
 	fmt.Printf("Total memory: %d\n", memory.TotalMemory())
-	return uint64(float64(memory.TotalMemory()) * 0.8 / float64(itemSize))
+	return uint64(float64(memory.TotalMemory()) * 0.6 / float64(itemSize))
 }
 
 func verifyHashesStoredWithNodes[N any](
