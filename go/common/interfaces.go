d3a5136bcc5eaad341db68b1c88ff10a4a040fe7
Bernhard Scholz <bernhard@fantom.foundation>
2024-04-16 08:31:26 +0200

    Add short header for BSL license

--- go/common/interfaces.go
+++ go/common/interfaces.go
@@ -1,3 +1,15 @@
+//
+// Copyright (c) 2024 Fantom Foundation
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE.TXT file and at fantom.foundation/bsl11.
+//
+// Change Date: 2028-4-16
+//
+// On the date above, in accordance with the Business Source License, use
+// of this software will be governed by the GNU Lesser General Public Licence v3 
+//
+
 package common
 
 import (
