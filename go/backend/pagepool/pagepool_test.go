2e581946fe5bb0372d16e961ea2f1c754b3f70d2
Kamil Jezek <kamil.jezek@gmail.com>
2024-04-16 09:05:38 +0200

    add comma everywhere

--- go/backend/pagepool/pagepool_test.go
+++ go/backend/pagepool/pagepool_test.go
@@ -7,7 +7,7 @@
 // Change Date: 2028-4-16
 //
 // On the date above, in accordance with the Business Source License, use
-// of this software will be governed by the GNU Lesser General Public Licence v3 
+// of this software will be governed by the GNU Lesser General Public Licence v3.
 //
 
 package pagepool
