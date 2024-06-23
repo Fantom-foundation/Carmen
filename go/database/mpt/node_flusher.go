ca0fa8047c4492a5daeb15e87d345b7dc9a928e8
Herbert Jordan <herbert@swrnd.ch>
2024-06-23 10:48:01 +0200

    Add worker flushing nodes in the background

---
+++ go/database/mpt/node_flusher.go
@@ -0,0 +1,125 @@
+// Copyright (c) 2024 Fantom Foundation
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file and at fantom.foundation/bsl11.
+//
+// Change Date: 2028-4-16
+//
+// On the date above, in accordance with the Business Source License, use of
+// this software will be governed by the GNU Lesser General Public License v3.
+
+package mpt
+
+import (
+	"errors"
+	"slices"
+	"time"
+
+	"github.com/Fantom-foundation/Carmen/go/database/mpt/shared"
+)
+
+type nodeFlusher struct {
+	shutdown chan<- struct{}
+	done     <-chan struct{}
+	errs     []error
+}
+
+type nodeFlusherConfig struct {
+	period time.Duration
+}
+
+func startNodeFlusher(cache NodeCache, sink NodeSink) *nodeFlusher {
+	return startNodeFlusherWithConfig(cache, sink, nodeFlusherConfig{
+		period: 5 * time.Second,
+	})
+}
+
+func startNodeFlusherWithConfig(cache NodeCache, sink NodeSink, config nodeFlusherConfig) *nodeFlusher {
+
+	shutdown := make(chan struct{})
+	done := make(chan struct{})
+
+	res := &nodeFlusher{
+		shutdown: shutdown,
+		done:     done,
+	}
+	go func() {
+		defer close(done)
+		ticker := time.NewTicker(config.period)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-shutdown:
+				return
+			case <-ticker.C:
+				if err := tryFlushDirtyNodes(cache, sink); err != nil {
+					res.errs = append(res.errs, err)
+				}
+			}
+		}
+	}()
+
+	return res
+}
+
+func (f *nodeFlusher) Stop() error {
+	close(f.shutdown)
+	<-f.done
+	return errors.Join(f.errs...)
+}
+
+func tryFlushDirtyNodes(cache NodeCache, sink NodeSink) error {
+	// Collect a list of dirty nodes to be flushed.
+	dirtyIds := make([]NodeId, 0, 1_000_000)
+	cache.ForEach(func(id NodeId, node *shared.Shared[Node]) {
+		handle, success := node.TryGetViewHandle()
+		if !success {
+			return
+		}
+		dirty := handle.Get().IsDirty()
+		handle.Release()
+		if !dirty {
+			return
+		}
+		dirtyIds = append(dirtyIds, id)
+	})
+
+	// The IDs are sorted to increase the chance of sequential
+	// writes to the disk.
+	slices.Sort(dirtyIds)
+
+	var errs []error
+	for _, id := range dirtyIds {
+		ref := NewNodeReference(id)
+		node, success := cache.Get(&ref)
+		if !success {
+			continue
+		}
+
+		// This service is a best-effort service. If the
+		// node is in use right now, we skip the flush and
+		// continue with the next node.
+		handle, success := node.TryGetWriteHandle()
+		if !success {
+			continue
+		}
+
+		// Nodes with dirtyHash hashes can not be flushed.
+		_, dirtyHash := handle.Get().GetHash()
+		if dirtyHash {
+			handle.Release()
+			continue
+		}
+
+		err := sink.Write(id, handle.AsViewHandle())
+		if err == nil {
+			handle.Get().MarkClean()
+		}
+		handle.Release()
+
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	return errors.Join(errs...)
+}
---
+++ go/database/mpt/node_flusher_test.go
@@ -0,0 +1,322 @@
+// Copyright (c) 2024 Fantom Foundation
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file and at fantom.foundation/bsl11.
+//
+// Change Date: 2028-4-16
+//
+// On the date above, in accordance with the Business Source License, use of
+// this software will be governed by the GNU Lesser General Public License v3.
+
+package mpt
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/Fantom-foundation/Carmen/go/database/mpt/shared"
+	"go.uber.org/mock/gomock"
+)
+
+func TestNodeFlusher_StartAndStop(t *testing.T) {
+	flusher := startNodeFlusher(nil, nil)
+	if err := flusher.Stop(); err != nil {
+		t.Fatalf("failed to stop node flusher: %v", err)
+	}
+}
+
+func TestNodeFlusher_TriggersFlushesPeriodically(t *testing.T) {
+	const period = time.Millisecond * 100
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+
+	flushSignal := make(chan struct{}, 1)
+	cache.EXPECT().ForEach(gomock.Any()).Times(3).Do(func(f func(id NodeId, node *shared.Shared[Node])) {
+		flushSignal <- struct{}{}
+	}).Return()
+
+	flusher := startNodeFlusherWithConfig(cache, nil, nodeFlusherConfig{
+		period: period,
+	})
+
+	last := time.Now()
+	for i := 0; i < 3; i++ {
+		select {
+		case <-flushSignal:
+			if time.Since(last) < period/2 {
+				t.Fatalf("flush signal received too early")
+			}
+			last = time.Now()
+		case <-time.After(period * 2):
+			t.Fatalf("flush signal not received")
+		}
+	}
+
+	if err := flusher.Stop(); err != nil {
+		t.Fatalf("failed to stop node flusher: %v", err)
+	}
+}
+
+func TestNodeFlusher_ErrorsAreCollected(t *testing.T) {
+	const period = time.Millisecond
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	id := ValueId(1)
+	node := shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}})
+
+	done := make(chan struct{})
+	counter := 0
+	cache.EXPECT().ForEach(gomock.Any()).AnyTimes().Do(func(f func(NodeId, *shared.Shared[Node])) {
+		f(id, node)
+		counter++
+		if counter == 3 {
+			close(done)
+		}
+	}).Return()
+
+	injectedError := fmt.Errorf("injected error")
+	cache.EXPECT().Get(RefTo(id)).Return(node, true).AnyTimes()
+	sink.EXPECT().Write(id, gomock.Any()).Return(injectedError).AnyTimes()
+
+	flusher := startNodeFlusherWithConfig(cache, sink, nodeFlusherConfig{
+		period: period,
+	})
+
+	<-done
+
+	if err := flusher.Stop(); !errors.Is(err, injectedError) {
+		t.Errorf("expected injected error, got: %v", err)
+	}
+}
+
+func TestNodeFlusher_FlushesOnlyDirtyNodes(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	nodes := map[NodeId]*shared.Shared[Node]{
+		ValueId(1): shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: true}}),
+		ValueId(2): shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}}),
+		ValueId(3): shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}}),
+		ValueId(4): shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: true}}),
+	}
+
+	// All nodes are checked.
+	cache.EXPECT().ForEach(gomock.Any()).Do(func(f func(NodeId, *shared.Shared[Node])) {
+		for i, node := range nodes {
+			f(i, node)
+		}
+	})
+
+	// Only the dirty ones are fetched.
+	cache.EXPECT().Get(RefTo(ValueId(2))).Return(nodes[ValueId(2)], true)
+	cache.EXPECT().Get(RefTo(ValueId(3))).Return(nodes[ValueId(3)], true)
+
+	// Only the dirty nodes are flushed.
+	sink.EXPECT().Write(ValueId(2), gomock.Any())
+	sink.EXPECT().Write(ValueId(3), gomock.Any())
+
+	if err := tryFlushDirtyNodes(cache, sink); err != nil {
+		t.Fatalf("failed to flush dirty nodes: %v", err)
+	}
+
+	for _, node := range nodes {
+		checkThatNodeIsNotLocked(t, node)
+	}
+}
+
+func TestNodeFlusher_FlushedNodesAreMarkedClean(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	id := ValueId(1)
+	node := shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}})
+	cache.EXPECT().ForEach(gomock.Any()).Do(func(f func(NodeId, *shared.Shared[Node])) {
+		f(id, node)
+	})
+
+	cache.EXPECT().Get(RefTo(id)).Return(node, true)
+	sink.EXPECT().Write(id, gomock.Any())
+
+	if err := tryFlushDirtyNodes(cache, sink); err != nil {
+		t.Fatalf("failed to flush dirty nodes: %v", err)
+	}
+
+	handle := node.GetViewHandle()
+	if dirty := handle.Get().IsDirty(); dirty {
+		t.Errorf("flushed node was not marked as clean")
+	}
+	handle.Release()
+	checkThatNodeIsNotLocked(t, node)
+}
+
+func TestNodeFlusher_NodesInUseAreIgnored(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	id := ValueId(1)
+	node := shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}})
+	cache.EXPECT().ForEach(gomock.Any()).Do(func(f func(NodeId, *shared.Shared[Node])) {
+		f(id, node)
+	})
+
+	// There shall be no write events (which is the default, but spelled out explicitly here).
+	sink.EXPECT().Write(gomock.Any(), gomock.Any()).Times(0)
+
+	// Get a lock on the node, which should prevent it from being flushed.
+	handle := node.GetWriteHandle()
+
+	if err := tryFlushDirtyNodes(cache, sink); err != nil {
+		t.Fatalf("failed to flush dirty nodes: %v", err)
+	}
+
+	if dirty := handle.Get().IsDirty(); !dirty {
+		t.Errorf("the test node should still be marked as dirty")
+	}
+
+	handle.Release()
+	checkThatNodeIsNotLocked(t, node)
+}
+
+func TestNodeFlusher_NodesThatAreAccessedAfterBeingIdentifiedAsDirtyAreIgnored(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	id := ValueId(1)
+	node := shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}})
+	handle := shared.WriteHandle[Node]{}
+	cache.EXPECT().ForEach(gomock.Any()).Do(func(f func(NodeId, *shared.Shared[Node])) {
+		f(id, node)
+		handle = node.GetWriteHandle() // < lock this node after it was identified as dirty
+	})
+
+	cache.EXPECT().Get(RefTo(id)).Return(node, true)
+
+	// There shall be no write events (which is the default, but spelled out explicitly here).
+	sink.EXPECT().Write(gomock.Any(), gomock.Any()).Times(0)
+
+	if err := tryFlushDirtyNodes(cache, sink); err != nil {
+		t.Fatalf("failed to flush dirty nodes: %v", err)
+	}
+
+	handle.Release()
+	checkThatNodeIsNotLocked(t, node)
+}
+
+func TestNodeFlusher_EvictedNodesAreIgnored(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	id := ValueId(1)
+	node := shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}})
+	cache.EXPECT().ForEach(gomock.Any()).Do(func(f func(NodeId, *shared.Shared[Node])) {
+		f(id, node)
+	})
+
+	// The node gets evicted between the for-each and the lookup.
+	cache.EXPECT().Get(RefTo(id)).Return(nil, false)
+
+	// There shall be no write events (which is the default, but spelled out explicitly here).
+	sink.EXPECT().Write(gomock.Any(), gomock.Any()).Times(0)
+
+	if err := tryFlushDirtyNodes(cache, sink); err != nil {
+		t.Fatalf("failed to flush dirty nodes: %v", err)
+	}
+
+	checkThatNodeIsNotLocked(t, node)
+}
+
+func TestNodeFlusher_NodesWithDiryHashesAreIgnored(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	id := ValueId(1)
+	node := shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false, hashStatus: hashStatusDirty}})
+	cache.EXPECT().ForEach(gomock.Any()).Do(func(f func(NodeId, *shared.Shared[Node])) {
+		f(id, node)
+	})
+
+	cache.EXPECT().Get(RefTo(id)).Return(node, true)
+
+	// There shall be no write events (which is the default, but spelled out explicitly here).
+	sink.EXPECT().Write(gomock.Any(), gomock.Any()).Times(0)
+
+	if err := tryFlushDirtyNodes(cache, sink); err != nil {
+		t.Fatalf("failed to flush dirty nodes: %v", err)
+	}
+
+	checkThatNodeIsNotLocked(t, node)
+}
+
+func TestNodeFlusher_FlushErrorsArePropagated(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	id := ValueId(1)
+	node := shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}})
+	cache.EXPECT().ForEach(gomock.Any()).Do(func(f func(NodeId, *shared.Shared[Node])) {
+		f(id, node)
+	})
+
+	injectedError := fmt.Errorf("injected error")
+	cache.EXPECT().Get(RefTo(id)).Return(node, true)
+	sink.EXPECT().Write(id, gomock.Any()).Return(injectedError)
+
+	if err := tryFlushDirtyNodes(cache, sink); !errors.Is(err, injectedError) {
+		t.Errorf("expected injected error, got: %v", err)
+	}
+
+	handle := node.GetViewHandle()
+	if dirty := handle.Get().IsDirty(); !dirty {
+		t.Errorf("failed flush should not lead to node marked as clean")
+	}
+	handle.Release()
+	checkThatNodeIsNotLocked(t, node)
+}
+
+func TestNodeFlusher_FlushErrorsAreAggregated(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cache := NewMockNodeCache(ctrl)
+	sink := NewMockNodeSink(ctrl)
+
+	id1 := ValueId(1)
+	id2 := ValueId(2)
+	node := shared.MakeShared[Node](&ValueNode{nodeBase: nodeBase{clean: false}})
+	cache.EXPECT().ForEach(gomock.Any()).Do(func(f func(NodeId, *shared.Shared[Node])) {
+		f(id1, node)
+		f(id2, node)
+	})
+
+	cache.EXPECT().Get(RefTo(id1)).Return(node, true)
+	cache.EXPECT().Get(RefTo(id2)).Return(node, true)
+
+	injectedError1 := fmt.Errorf("injected error - 1")
+	injectedError2 := fmt.Errorf("injected error - 2")
+	sink.EXPECT().Write(id1, gomock.Any()).Return(injectedError1)
+	sink.EXPECT().Write(id2, gomock.Any()).Return(injectedError2)
+
+	if err := tryFlushDirtyNodes(cache, sink); !errors.Is(err, injectedError1) || !errors.Is(err, injectedError2) {
+		t.Errorf("expected injected errors, got: %v", err)
+	}
+
+	checkThatNodeIsNotLocked(t, node)
+}
+
+func checkThatNodeIsNotLocked(t *testing.T, node *shared.Shared[Node]) {
+	t.Helper()
+	handle, success := node.TryGetWriteHandle()
+	if !success {
+		t.Errorf("node is locked")
+	}
+	handle.Release()
+}
