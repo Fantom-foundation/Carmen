215e62827c093a476303d72f7989505266c52f48
Herbert Jordan <herbert@swrnd.ch>
2024-07-22 21:59:40 +0200

    Add end-to-end test for code checkpoints

--- go/database/mpt/codes_test.go
+++ go/database/mpt/codes_test.go
@@ -11,8 +11,11 @@
 package mpt
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/Fantom-foundation/Carmen/go/backend/utils"
 )
 
 func TestCodes_OpenCodes(t *testing.T) {
@@ -52,5 +55,65 @@ func TestCodes_CodesCanBeAddedAndRetrieved(t *testing.T) {
 
 	if want, got := code2, codes.getCodeForHash(hash2); string(want) != string(got) {
 		t.Fatalf("expected code2, got %s", got)
+	}
+}
+
+func TestCodes_CheckpointsCanBeRestored(t *testing.T) {
+	dir := t.TempDir()
+	file := filepath.Join(dir, "codes.dat")
+	codes, err := openCodes(file, dir)
+	if err != nil {
+		t.Fatalf("failed to open codes: %v", err)
+	}
+
+	codes.add([]byte("code1"))
+	codes.add([]byte("code2"))
+
+	checkpoint := utils.Checkpoint(1)
+	if err := codes.Prepare(checkpoint); err != nil {
+		t.Fatalf("failed to prepare checkpoint: %v", err)
+	}
+
+	if err := codes.Commit(checkpoint); err != nil {
+		t.Fatalf("failed to commit checkpoint: %v", err)
+	}
+
+	backup, err := os.Stat(file)
+	if err != nil {
+		t.Fatalf("failed to stat file: %v", err)
+	}
+
+	codes.add([]byte("code3"))
+	if want, got := 3, len(codes.codes); want != got {
+		t.Fatalf("expected codes to have %d entries, got %d", want, got)
+	}
+
+	if err := codes.Flush(); err != nil {
+		t.Fatalf("failed to flush: %v", err)
+	}
+
+	modified, err := os.Stat(file)
+	if err != nil {
+		t.Fatalf("failed to stat file: %v", err)
+	}
+
+	if modified.Size() <= backup.Size() {
+		t.Fatalf("expected file to be larger after flush")
+	}
+
+	if err := codes.Restore(checkpoint); err != nil {
+		t.Fatalf("failed to restore checkpoint: %v", err)
+	}
+	if want, got := 2, len(codes.codes); want != got {
+		t.Fatalf("expected codes to have %d entries, got %d", want, got)
+	}
+
+	restored, err := os.Stat(file)
+	if err != nil {
+		t.Fatalf("failed to stat file: %v", err)
+	}
+
+	if restored.Size() != backup.Size() {
+		t.Fatalf("expected file to be same size after restore")
 	}
 }
