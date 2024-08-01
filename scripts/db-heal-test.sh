25ca64f7e35ec6ccafd15503f4d13f80f26c4781
Petr Hanzl <petrhanzl33@gmail.com>
2024-08-01 13:27:08 +0200

    Complete the testing script

--- scripts/db-heal-test.sh
+++ scripts/db-heal-test.sh
@@ -59,13 +59,24 @@ wait $command_pid
 
 # Find working dir
 working_dir=$(ls -td "$tmp_path"/*/ | head -1)
+archive="${working_dir}archive"
+live="${working_dir}live"
 
-(cd $carmen_root && go run ./database/mpt/tool reset --force-unlock "$working_dir"archive "$restore_block")
+(cd $carmen_root && go run ./database/mpt/tool reset --force-unlock "$archive" "$restore_block")
 
-echo "Restoration complete. Syncing to block $final_block."
+genesis="${tmp_path}test_genesis.dat"
 
-final_first=$((restore+1))
-(cd $aida_path && ./build/aida-vm-sdb substate --validate --db-tmp "$tmp_path" --carmen-schema 5 --db-impl carmen --aida-db "$aida_db_path" --no-heartbeat-logging --track-progress --archive --archive-variant s5 --archive-query-rate 200 --db-src "$working_dir" "$final_first" "$final_block")
+echo "Restoration complete. Exporting LiveDB genesis."
+(cd $carmen_root && go run ./database/mpt/tool export --block "$restore_block" "$archive" "$genesis")
+
+echo "Export complete. Applying LiveDB genesis."
+rm -rf "$live"
+(cd $carmen_root && go run ./database/mpt/tool import-live-db "$genesis" "$live")
+
+echo "Syncing to block "$final_block""
+final_first=$((restore_block+1))
+cmd="./build/aida-vm-sdb substate --validate --db-tmp "$tmp_path" --carmen-schema 5 --db-impl carmen --aida-db "$aida_db_path" --no-heartbeat-logging --track-progress --archive --archive-variant s5 --archive-query-rate 200 --db-src "$working_dir" --skip-priming "$final_first" "$final_block""
+(cd $aida_path && $cmd >> "$log_file")
 
 echo "Sync complete to block $final_block. Final db path: $(ls -td "$tmp_path"/*/ | head -1)."
 
