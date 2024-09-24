53b8bfa52a0389d4d9a0a3b4abfcbab91e667581
Petr Hanzl <petrhanzl33@gmail.com>
2024-09-24 15:53:13 +0200

    Address reviewers feedback; Fix leaks in TestStateDBArchive

--- go/database/mpt/diagnostics_test.go
+++ go/database/mpt/diagnostics_test.go
@@ -8,7 +8,7 @@
 // On the date above, in accordance with the Business Source License, use of
 // this software will be governed by the GNU Lesser General Public License v3.
 
-package state
+package mpt
 
 import (
 	"fmt"
